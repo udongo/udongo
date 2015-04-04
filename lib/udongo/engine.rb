ef5442c3f1c11e52e9c1d7400e11a829d56a7107
Bauffman <davy@bauffman.be>
2015-04-04 15:30:46 +0200

    Make sure rspec is used when invoking generators.

--- lib/udongo/engine.rb
+++ lib/udongo/engine.rb
@@ -1,4 +1,10 @@
 module Udongo
   class Engine < ::Rails::Engine
+
+    # Let Rails generators create FactoryGirl factories.
+    config.generators do |g|
+      g.test_framework :rspec, fixture: false
+      g.fixture_replacement :factory_girl, dir: 'spec/factories'
+    end
   end
 end

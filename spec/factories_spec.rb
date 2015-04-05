8f93ca1b93d70487356cae13f1e430599ace756e
Bauffman <davy@bauffman.be>
2015-04-05 22:14:42 +0200

    Re-enable general factory testing.

--- spec/factories_spec.rb
+++ spec/factories_spec.rb
@@ -1,9 +1,9 @@
-# require 'rails_helper'
-#
-# describe 'Factories' do
-#   FactoryGirl.factories.map(&:name).each do |factory_name|
-#     it "#{factory_name} is valid" do
-#       expect(build(factory_name).save!).to be true
-#     end
-#   end
-# end
+require 'rails_helper'
+
+describe 'Factories' do
+  FactoryGirl.factories.map(&:name).each do |factory_name|
+    it "#{factory_name} is valid" do
+      expect(build(factory_name).save!).to be true
+    end
+  end
+end

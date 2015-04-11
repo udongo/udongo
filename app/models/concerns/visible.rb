e906b22a9acea46cac2734487e15b59354c8e113
Bauffman <davy@bauffman.be>
2015-04-11 17:16:37 +0200

    Add the visible concern.

---
+++ app/models/concerns/visible.rb
@@ -0,0 +1,24 @@
+# If you want to use this concern, you need to add a boolean field 'visible' to
+# the model.
+module Concerns
+  module Visible
+    extend ActiveSupport::Concern
+
+    included do
+      scope :visible, ->{ where(visible: true) }
+      scope :hidden, ->{ where('visible = 0 OR visible IS NULL') }
+    end
+
+    def hidden?
+      !visible?
+    end
+
+    def hide!
+      update_attribute :visible, false
+    end
+
+    def show!
+      update_attribute :visible, true
+    end
+  end
+end

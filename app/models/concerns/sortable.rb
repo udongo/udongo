8d7a2740b3a0cf188332516967de2da201590d85
Bauffman <davy@bauffman.be>
2015-04-11 17:13:53 +0200

    Add the sortable concern.

---
+++ app/models/concerns/sortable.rb
@@ -0,0 +1,30 @@
+# If you want to use this concern, you need to add an integer field 'position'
+# to the model.
+module Concerns
+  module Sortable
+    extend ActiveSupport::Concern
+
+    included do
+      acts_as_list
+      default_scope ->{ order(:position) }
+    end
+
+    module ClassMethods
+      def sortable(*args)
+        acts_as_list *args
+      end
+    end
+
+    def set_position(new_position, parent_id = nil)
+      if respond_to?(:draggable?) && !draggable? || position == new_position.to_i
+        return false
+      end
+
+      if respond_to?(:parentable?) && parentable?
+        update_attribute :parent_id, parent_id
+      end
+
+      set_list_position new_position
+    end
+  end
+end
---
+++ spec/support/concerns/sortable.rb
@@ -0,0 +1,73 @@
+require 'rails_helper'
+
+shared_examples_for :sortable do
+  let(:model) { described_class }
+  let(:klass) { model.to_s.underscore.to_sym }
+
+  describe 'scopes' do
+    it :default_scope do
+      a = create(klass, position: 2)
+      b = create(klass, position: 3)
+      c = create(klass, position: 1)
+      expect(model.all).to eq [c, a, b]
+      pending
+    end
+
+    it 'new records' do
+      a = create(klass)
+      b = create(klass)
+      c = create(klass)
+      expect(model.all).to eq [a, b, c]
+      pending
+    end
+  end
+
+  describe '#set_position' do
+    before :each do
+      @first = create klass, position: 1
+      if @first.scope_condition.is_a?(Hash)
+        @second = create klass, @first.scope_condition.merge!({ position: 2 })
+      else
+        @second = create klass, position: 2
+      end
+      pending
+    end
+
+    it :parentable do
+      if @first.respond_to?(:parentable) && @first.parentable? && @first.scope_name == 'parent_id'
+        two = create(klass, parent_id: @first.id)
+        three = create(klass)
+
+        three.set_position 2, @first.id
+        expect(three.parent_id).to eq @first.id
+        pending
+      end
+    end
+
+    context 'draggable' do
+      it :with do
+        if @first.respond_to?(:draggable)
+          @first.update_attribute :draggable, false
+          @first.set_position 2
+
+          expect(model.find(@first.id).position).to eq 1
+          expect(model.find(@second.id).position).to eq 2
+        end
+        pending
+      end
+
+      # You can't test the state without draggable/parentable reliably, due
+      # to scope issues.
+    end
+  end
+
+  it '.respond_to?' do
+    expect(model).to respond_to(:acts_as_list, :sortable)
+    pending
+  end
+
+  it '#respond_to?' do
+    expect(model.new).to respond_to(:set_position, :set_list_position)
+    pending
+  end
+end

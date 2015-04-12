e928b143e2757a0ca5870f72b68a785f68551d6d
Bauffman <davy@bauffman.be>
2015-04-12 14:11:47 +0200

    Slug uniqueness should move to the actual forms.

--- app/models/meta.rb
+++ app/models/meta.rb
@@ -1,10 +1,7 @@
 class Meta < ActiveRecord::Base
   belongs_to :sluggable, polymorphic: true
 
-  validates :locale, presence: true
-  validates :slug,
-            presence: true,
-            uniqueness: { case_sensitive: false, scope: :locale }
+  validates :locale, :slug, presence: true
 
   %w(title keywords description custom slug).each do |field|
     alias_attribute "seo_#{field}".to_sym, field
--- spec/models/meta_spec.rb
+++ spec/models/meta_spec.rb
@@ -9,11 +9,6 @@ describe Meta do
       it(:slug) { expect(build(klass, slug: nil)).not_to be_valid }
       it(:locale) { expect(build(klass, locale: nil)).not_to be_valid }
     end
-
-    it 'unique slug' do
-      create(klass, slug: 'foo', sluggable_type: 'Bar')
-      expect(build(klass, slug: 'FOO', sluggable_type: 'BAR')).not_to be_valid
-    end
   end
 
   it '#respond_to?' do

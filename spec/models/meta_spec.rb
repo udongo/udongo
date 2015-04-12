42e38f04c252961c29358782818cce89eb230a82
Bauffman <davy@bauffman.be>
2015-04-12 14:09:06 +0200

    Tweak the meta spec.

--- spec/models/meta_spec.rb
+++ spec/models/meta_spec.rb
@@ -6,18 +6,21 @@ describe Meta do
 
   describe 'validations' do
     describe 'presence' do
-      it(:slug) { expect(build(klass, slug: nil)).to_not be_valid }
-      it(:locale) { expect(build(klass, locale: nil)).to_not be_valid }
+      it(:slug) { expect(build(klass, slug: nil)).not_to be_valid }
+      it(:locale) { expect(build(klass, locale: nil)).not_to be_valid }
     end
 
     it 'unique slug' do
       create(klass, slug: 'foo', sluggable_type: 'Bar')
-      expect(build(klass, slug: 'FOO', sluggable_type: 'BAR')).to_not be_valid
+      expect(build(klass, slug: 'FOO', sluggable_type: 'BAR')).not_to be_valid
     end
   end
 
   it '#respond_to?' do
-    expect(model.new).to respond_to(:sluggable)
+    expect(model.new).to respond_to(
+      :sluggable, :seo_title, :seo_keywords, :seo_description, :seo_custom,
+      :seo_slug
+    )
   end
 end
 

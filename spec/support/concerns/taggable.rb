90c4c572f011aa8e31465a1bda5137b2f1c83618
Bauffman <davy@bauffman.be>
2015-04-17 13:31:24 +0200

    Add taggable concern.

---
+++ spec/support/concerns/taggable.rb
@@ -0,0 +1,51 @@
+require 'rails_helper'
+
+shared_examples_for :taggable do
+  let(:model) { described_class }
+  let(:klass) { model.to_s.underscore.to_sym }
+  let(:instance) { create(klass) }
+
+  it '#tags' do
+    tag = create(:tag, locale: :nl, name: 'foo', slug: 'foo')
+    instance.tagged_items.create!(tag: tag)
+    expect(instance.tags).to eq [tag]
+    pending
+  end
+
+  it '#tags_string' do
+    tag1 = create(:tag, locale: :nl, name: 'foo', slug: 'foo')
+    tag2 = create(:tag, locale: :nl, name: 'bar', slug: 'bar')
+    instance.tagged_items.create!(tag: tag1)
+    instance.tagged_items.create!(tag: tag2)
+    expect(instance.tags_string).to eq 'foo,bar'
+    pending
+  end
+
+  describe '#related' do
+    let(:instance2) { create(klass) }
+    let(:tag1) { create(:tag, locale: :nl, name: 'foo', slug: 'foo') }
+    let(:tag2) { create(:tag, locale: :nl, name: 'bar', slug: 'bar') }
+
+    it :blank do
+      instance.tagged_items.create!(tag: tag1)
+      instance2.tagged_items.create!(tag: tag2)
+      expect(instance.related).to eq []
+      pending
+    end
+
+    it :results do
+      instance.tagged_items.create!(tag: tag1)
+      instance.tagged_items.create!(tag: tag2)
+      instance2.tagged_items.create!(tag: tag1)
+      instance2.tagged_items.create!(tag: tag2)
+      expect(instance.related).to eq [instance2]
+      expect(instance2.related).to eq [instance]
+      pending
+    end
+  end
+
+  it '#respond_to?' do
+    expect(model.new).to respond_to(:tagged_items, :related, :tags, :tags_string)
+  end
+end
+

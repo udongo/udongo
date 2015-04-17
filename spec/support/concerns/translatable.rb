37de15aa6bd0344687135a61de43e107de261676
Bauffman <davy@bauffman.be>
2015-04-17 14:18:23 +0200

    Add missing respond_to check for translatable.

--- spec/support/concerns/translatable.rb
+++ spec/support/concerns/translatable.rb
@@ -4,6 +4,10 @@ shared_examples_for :translatable do
   let(:model) { described_class }
   let(:klass) { model.to_s.underscore.to_sym }
 
+  it '#respond_to?' do
+    expect(build(klass)).to respond_to(:translation)
+  end
+
   it '.respond_to?' do
     expect(model).to respond_to(
       :translatable_field, :translatable_fields, :translation_config

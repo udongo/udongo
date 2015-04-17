7e1e088a031fb3ebedfff277fad8aee43d51107f
davelens <github@davelens.be>
2015-04-17 14:38:01 +0200

    add IconHelper#icon_and_label

--- app/helpers/backend/icon_helper.rb
+++ app/helpers/backend/icon_helper.rb
@@ -3,5 +3,9 @@ module Backend
     def icon(name)
       content_tag :i, nil, class: "fi-#{name.to_s.gsub('_', '-')}"
     end
+
+    def icon_and_label(name, label)
+      "#{icon(name)}&nbsp;#{I18n.t(label)}".html_safe
+    end
   end
 end

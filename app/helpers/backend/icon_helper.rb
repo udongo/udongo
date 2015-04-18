f4475664eee77242ae20c95ca9139307d4e8c795
Bauffman <davy@bauffman.be>
2015-04-18 22:01:30 +0200

    Make the icon method more flexible.
    * I don't think it's a good idea to always assume the label is going to
      be an I18n key.
    * One method instead of two.

--- app/helpers/backend/icon_helper.rb
+++ app/helpers/backend/icon_helper.rb
@@ -1,11 +1,8 @@
 module Backend
   module IconHelper
-    def icon(name)
-      content_tag :i, nil, class: "fi-#{name.to_s.gsub('_', '-')}"
-    end
-
-    def icon_and_label(name, label)
-      "#{icon(name)}&nbsp;#{I18n.t(label)}".html_safe
+    def icon(name, label = nil)
+      markup = content_tag :i, nil, class: "fi-#{name.to_s.gsub('_', '-')}"
+      label ? "#{markup}&nbsp;#{label}".html_safe : markup
     end
   end
 end

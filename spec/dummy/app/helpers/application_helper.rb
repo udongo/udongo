3320de3d3f10014df3eb6b79212235ceee56772f
davelens <github@davelens.be>
2015-05-15 14:30:16 +0200

    add ApplicationHelper (and move `present` method from spec/dummy)

--- spec/dummy/app/helpers/application_helper.rb
+++ spec/dummy/app/helpers/application_helper.rb
@@ -1,8 +1,2 @@
 module ApplicationHelper
-  def present(object, klass = nil)
-    klass ||= "#{object.class}Presenter".constantize
-    presenter = klass.new(object, self)
-    yield presenter if block_given?
-    presenter
-  end
 end

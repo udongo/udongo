5e527e2f0c62ad5d5d8a3df5ee6c3fc81c9f8ecb
davelens <github@davelens.be>
2015-05-15 14:15:53 +0200

    refactor BasePresenter

--- app/presenters/base_presenter.rb
+++ app/presenters/base_presenter.rb
@@ -4,16 +4,10 @@ class BasePresenter
     @template = template
   end
 
-  private
-
   def self.presents(name)
     define_method(name) do
       @object
     end
-  end
-
-  def h
-    @template
   end
 
   def method_missing(*args, &block)

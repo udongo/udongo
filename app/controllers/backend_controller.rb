17e03f43f5214e8dbf7af2af635e8c577527e885
davelens <github@davelens.be>
2015-05-21 16:50:47 +0200

    alphabetize and add BackendController#translate_notice

--- app/controllers/backend_controller.rb
+++ app/controllers/backend_controller.rb
@@ -2,24 +2,6 @@ class BackendController < ActionController::Base
   layout 'backend/application'
   before_action :default_locale, :check_login
 
-  private
-
-  def default_locale
-    I18n.locale = :nl
-  end
-
-  def current_admin
-    @current_admin ||= Admin.find(session[:admin_id]) if session[:admin_id]
-  end
-  helper_method :current_admin
-
-  def check_login
-    unless current_admin
-      session[:backend_redirect] = request.url
-      redirect_to new_backend_session_path
-    end
-  end
-
   def breadcrumb
     unless @breadcrumb
       @breadcrumb = Udongo::Breadcrumb.new
@@ -30,4 +12,26 @@ class BackendController < ActionController::Base
     @breadcrumb
   end
   helper_method :breadcrumb
+
+  def current_admin
+    @current_admin ||= Admin.find(session[:admin_id]) if session[:admin_id]
+  end
+  helper_method :current_admin
+
+  def default_locale
+    I18n.locale = :nl
+  end
+
+  def translate_notice(notice, actor)
+    I18n.t("b.msg.#{notice}") % I18n.t("b.#{actor}")
+  end
+
+  private
+
+  def check_login
+    unless current_admin
+      session[:backend_redirect] = request.url
+      redirect_to new_backend_session_path
+    end
+  end
 end

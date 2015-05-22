85e52941191b5c1b5ea309148eb5dc3f1b44026d
davelens <github@davelens.be>
2015-05-22 15:24:14 +0200

    add missing Backend::PageTranslationForm

---
+++ app/forms/backend/page_translation_form.rb
@@ -0,0 +1,18 @@
+class Backend::PageTranslationForm < Reform::Form
+  include Composition
+
+  model :page
+
+  property :title, on: :translation
+  property :subtitle, on: :translation
+  property :content, on: :translation
+
+  property :seo_title, on: :seo
+  property :seo_keywords, on: :seo
+  property :seo_description, on: :seo
+  property :seo_custom, on: :seo
+  property :seo_slug, on: :seo
+
+  validates :title, presence: true
+  validates :seo_slug, presence: true
+end

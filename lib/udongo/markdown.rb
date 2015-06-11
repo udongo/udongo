893e15c7eb4d1b39cdd5241571dd181dae301968
Bauffman <davy@bauffman.be>
2015-06-11 14:31:01 +0200

    Force to string.

--- lib/udongo/markdown.rb
+++ lib/udongo/markdown.rb
@@ -3,7 +3,7 @@ require 'redcarpet'
 module Udongo
   class Markdown
     def self.to_html(string, options = {})
-      Redcarpet::Markdown.new(Redcarpet::Render::HTML, options).render(string).html_safe
+      Redcarpet::Markdown.new(Redcarpet::Render::HTML, options).render(string.to_s).html_safe
     end
   end
 end

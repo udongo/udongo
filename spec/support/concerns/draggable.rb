93f9e0b1a11f747f69795db8c83f1441b97d0e35
Bauffman <davy@bauffman.be>
2015-06-20 14:30:38 +0200

    Fix the draggable concern spec.

--- spec/support/concerns/draggable.rb
+++ spec/support/concerns/draggable.rb
@@ -7,25 +7,22 @@ shared_examples_for :draggable do
   describe 'after_intialize' do
     context 'new record' do
       it :true do
-        expect(build(klass).draggable).to be true
-        pending
+        expect(build(klass)).to be_draggable
       end
     end
 
     context 'existing record after create' do
       it :false do
         instance = create(klass, draggable: false)
-        expect(instance.draggable).to be false
-        pending
+        expect(instance).not_to be_draggable
       end
     end
 
     context 'existing record after save' do
       it :false do
-        instance = create klass
+        instance = create(klass)
         instance.update_attribute :draggable, false
-        expect(instance.draggable).to be false
-        pending
+        expect(instance).not_to be_draggable
       end
     end
   end

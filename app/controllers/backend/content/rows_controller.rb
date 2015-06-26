a6d4f058c1d076620d09756252bcfd85206cf2c1
Bauffman <davy@bauffman.be>
2015-06-26 13:50:12 +0200

    Tweak the new/destroy actions for content rows.

--- app/controllers/backend/content/rows_controller.rb
+++ app/controllers/backend/content/rows_controller.rb
@@ -2,12 +2,25 @@ class Backend::Content::RowsController < BackendController
   def new
     if params[:klass] && params[:id] && params[:locale]
       instance = params[:klass].constantize.find params[:id]
-      instance.content_rows.create!(locale: params[:locale])
+      row = instance.content_rows.create!(locale: params[:locale])
 
-      env["HTTP_REFERER"] += "#content-row-#{instance.content_rows.last.id}"
-      redirect_to :back
+      redirect_to content_path(instance, params[:locale], "content-row-#{row.id}")
     else
       render text: 'Insufficient params. Please provide klass, id and locale.'
     end
+  end
+
+  def destroy
+    row = ::ContentRow.find params[:id]
+    row.destroy
+
+    redirect_to content_path(row.rowable, row.locale, 'content-rows')
+  end
+
+  private
+
+  def content_path(instance, locale, anchor)
+    path = "edit_translation_backend_#{instance.class.to_s.downcase}_path"
+    send(path, instance, locale, anchor: anchor)
   end
 end

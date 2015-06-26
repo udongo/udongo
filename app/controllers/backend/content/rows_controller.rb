b5579cb8b1ff4b0538af61701d41e5107f535fd9
Bauffman <davy@bauffman.be>
2015-06-26 11:53:10 +0200

    Make it possible to add content rows.

--- app/controllers/backend/content/rows_controller.rb
+++ app/controllers/backend/content/rows_controller.rb
@@ -1,2 +1,13 @@
 class Backend::Content::RowsController < BackendController
+  def new
+    if params[:klass] && params[:id] && params[:locale]
+      instance = params[:klass].constantize.find params[:id]
+      instance.content_rows.create!(locale: params[:locale])
+
+      env["HTTP_REFERER"] += "#content-row-#{instance.content_rows.last.id}"
+      redirect_to :back
+    else
+      render text: 'Insufficient params. Please provide klass, id and locale.'
+    end
+  end
 end

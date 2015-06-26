766b8c3e719c57c0a73f4c772611f7ac5c1d88ec
Bauffman <davy@bauffman.be>
2015-06-26 14:23:52 +0200

    Get rid of the present method.

--- app/helpers/udongo_helper.rb
+++ app/helpers/udongo_helper.rb
@@ -6,13 +6,6 @@ module UdongoHelper
     content_for(target) { javascript_include_tag(file) }
   end
 
-  def present(object, klass = nil)
-    klass ||= "#{object.class}Presenter".constantize
-    presenter = klass.new(object, self)
-    yield presenter if block_given?
-    presenter
-  end
-
   # Before using: Put <%= yield(:stylesheets) %> in the <head> of your app's frontend
   def stylesheet(file, media = :screen)
     content_for(:stylesheets) { stylesheet_link_tag(file, media: media) }

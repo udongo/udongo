8c8edbeab0855ec77a388fcd7d96ac2c206d88a4
Bauffman <davy@bauffman.be>
2015-06-26 15:13:15 +0200

    Add todos.

--- app/models/content_text.rb
+++ app/models/content_text.rb
@@ -3,4 +3,9 @@ class ContentText < ActiveRecord::Base
   def display_me_mofo
     content.to_s.html_safe
   end
+
+  # TODO write me
+  def column
+    # TODO once written, implement in the texts_controller.rb file
+  end
 end

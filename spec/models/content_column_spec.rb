09c71a80b7cdaed40bb3395dc1676c71524a7a44
Bauffman <davy@bauffman.be>
2015-06-26 11:37:43 +0200

    Content rows are sortable.

--- spec/models/content_column_spec.rb
+++ spec/models/content_column_spec.rb
@@ -4,6 +4,8 @@ describe ContentColumn do
   let(:model) { described_class }
   let(:klass) { model.to_s.underscore.to_sym }
 
+  it_behaves_like :sortable
+
   describe 'validations' do
     describe 'presence' do
       it(:row) { expect(build(klass, row: nil)).not_to be_valid }

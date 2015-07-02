19bcd75b2a3c222e56b0d2528d7d470b67e679e1
Bauffman <davy@bauffman.be>
2015-07-02 14:17:14 +0200

    Make it possible to edit content images.

--- app/controllers/backend/content/rows/images_controller.rb
+++ app/controllers/backend/content/rows/images_controller.rb
@@ -1,13 +1,31 @@
 class Backend::Content::Rows::ImagesController < BackendController
-  def edit
-    raise 'foo'
-  end
+  before_action :find_image
+  layout 'backend/lightbox'
 
   def update
-    raise 'foo'
+    if @image.update_attributes allowed_params
+      redirect_to content_path
+    else
+      render :edit
+    end
   end
 
   def destroy
     raise 'foo'
   end
+
+  def find_image
+    @image = ::ContentImage.find params[:id]
+  end
+
+  def allowed_params
+    params.require(:content_image).permit(:file, :caption, :url)
+  end
+
+  def content_path
+    column = @image.column
+    path = "edit_translation_backend_#{column.row.rowable.class.to_s.downcase}_path"
+    send(path, column.row.rowable, locale, anchor: "content-row-#{column.row.id}")
+  end
+  helper_method :content_path
 end

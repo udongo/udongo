1954b773737378b9a0a4e9e71fb93423771f5498
Bauffman <davy@bauffman.be>
2015-07-02 09:16:19 +0200

    Add spec for the page decorator.

--- app/decorators/page_decorator.rb
+++ app/decorators/page_decorator.rb
@@ -1,12 +1,11 @@
 class PageDecorator < Draper::Decorator
   delegate_all
 
-  # TODO is this selected: nil still needed?
-  def options_for_parents(selected: nil, disabled: nil)
-    list = Page.flat_tree.map do |p|
+  def options_for_parents(disabled: nil)
+    list = ::Page.flat_tree.map do |p|
       ["#{'-' * p.depth} #{p.description}".strip, p.id]
     end
 
-    h.options_for_select(list, selected: selected, disabled: disabled)
+    h.options_for_select(list, selected: parent_id, disabled: disabled)
   end
 end

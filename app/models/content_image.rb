5a69bb96931f0af0fd7bc5bb34a654645ed9ead8
Bauffman <davy@bauffman.be>
2015-07-02 14:14:55 +0200

    Add todo.

--- app/models/content_image.rb
+++ app/models/content_image.rb
@@ -1,3 +1,8 @@
 class ContentImage < ActiveRecord::Base
   mount_uploader :file, ContentImageUploader
+
+  # TODO move to ContentType concern
+  def column
+    ::ContentColumn.where(content_type: self.class.name, content_id: id).take
+  end
 end
--- app/models/content_text.rb
+++ app/models/content_text.rb
@@ -1,4 +1,5 @@
 class ContentText < ActiveRecord::Base
+  # TODO move to ContentType concern
   def column
     ::ContentColumn.where(content_type: self.class.name, content_id: id).take
   end

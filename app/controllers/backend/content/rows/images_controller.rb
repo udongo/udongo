d1708ff77813fff7e54b8283016ab60b0054625e
Bauffman <davy@bauffman.be>
2015-07-02 16:48:11 +0200

    Refactor the images/texts controllers.

--- app/controllers/backend/content/rows/images_controller.rb
+++ app/controllers/backend/content/rows/images_controller.rb
@@ -1,29 +1,6 @@
 class Backend::Content::Rows::ImagesController < BackendController
-  before_action :find_model
-  layout 'backend/lightbox'
+  include Concerns::Backend::ContentTypeController
 
-  def update
-    if @model.update_attributes allowed_params
-      redirect_to content_path
-    else
-      render :edit
-    end
-  end
-
-  private
-
-  def find_model
-    @model = ::ContentImage.find params[:id]
-  end
-
-  def allowed_params
-    params.require(:content_image).permit(:file, :caption, :url)
-  end
-
-  def content_path
-    column = @model.column
-    path = "edit_translation_backend_#{column.row.rowable.class.to_s.downcase}_path"
-    send(path, column.row.rowable, locale, anchor: "content-row-#{column.row.id}")
-  end
-  helper_method :content_path
+  model ::ContentImage
+  allowed_params :file, :caption, :url
 end
---
+++ app/controllers/concerns/backend/content_type_controller.rb
@@ -0,0 +1,44 @@
+module Concerns
+  module Backend
+    module ContentTypeController
+      extend ActiveSupport::Concern
+
+      included do
+        layout 'backend/lightbox'
+        before_action :find_model
+        helper_method :content_path
+      end
+
+      module ClassMethods
+        def model(value)
+          define_method(:model) { value }
+        end
+
+        def allowed_params(*args)
+          define_method(:allowed_params) do
+            params.require(model.name.tableize.singularize).permit(*args)
+          end
+        end
+      end
+
+      def find_model
+        @model = model.find params[:id]
+      end
+
+      # TODO refactor?
+      def content_path
+        column = @model.column
+        path = "edit_translation_backend_#{column.row.rowable.class.to_s.downcase}_path"
+        send(path, column.row.rowable, locale, anchor: "content-row-#{column.row.id}")
+      end
+
+      def update
+        if @model.update_attributes allowed_params
+          redirect_to content_path
+        else
+          render :edit
+        end
+      end
+    end
+  end
+end

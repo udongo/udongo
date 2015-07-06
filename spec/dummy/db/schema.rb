39b0abe2b8e3825c84712ea5ea2d6b87135c072f
Bauffman <davy@bauffman.be>
2015-07-06 11:31:06 +0200

    Fix the snippets description column type.

--- spec/dummy/db/schema.rb
+++ spec/dummy/db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150706085213) do
+ActiveRecord::Schema.define(version: 20150706085733) do
 
   create_table "addresses", force: :cascade do |t|
     t.integer  "addressable_id",   limit: 4

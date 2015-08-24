d08ca4ce044a4ebb5550d1acc46cc982a12347ed
davelens <github@davelens.be>
2015-08-24 14:48:58 +0200

    add missing actions to Backend::RedirectsController

--- app/controllers/backend/redirects_controller.rb
+++ app/controllers/backend/redirects_controller.rb
@@ -13,10 +13,39 @@ class Backend::RedirectsController < BackendController
     @redirect = ::Redirect.new.decorate
   end
 
+  def create
+    @redirect = ::Redirect.new allowed_params
+
+    if @redirect.save
+      redirect_to backend_redirects_path, notice: translate_notice(:added, :redirect)
+    else
+      render :new
+    end
+  end
+
+  def destroy
+    @redirect.destroy
+    redirect_to backend_redirects_path, notice: translate_notice(:deleted, :redirect)
+  end
+
+  def update
+    if @redirect.update_attributes allowed_params
+      redirect_to backend_redirects_path, notice: translate_notice(:edited, :redirect)
+    else
+      render :edit
+    end
+  end
+
   private
 
+  def allowed_params
+    params.require(:redirect).permit(
+      :source_uri, :destination_uri, :status_code
+    )
+  end
+
   def find_model
-    @redirect = ::Redirect.find params[:id]
+    @redirect = ::Redirect.find(params[:id]).decorate
   end
 
   def translation_form

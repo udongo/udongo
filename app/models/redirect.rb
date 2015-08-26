95f4b281428290690a0c6ebf5debf21ce2498c49
Bauffman <davy@bauffman.be>
2015-08-26 09:48:29 +0200

    Use activerecord syntax.

--- app/models/redirect.rb
+++ app/models/redirect.rb
@@ -2,7 +2,7 @@ class Redirect < ActiveRecord::Base
   validates :source_uri, :destination_uri, :status_code, presence: true
   validates :source_uri, uniqueness: { case_sensitive: false }
 
-  scope :disabled, -> { where('disabled = 1') }
+  scope :disabled, -> { where(disabled: true) }
   scope :enabled, -> { where('disabled IS NULL or disabled = 0') }
 
   def enabled?

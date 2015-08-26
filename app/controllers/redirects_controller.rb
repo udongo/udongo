a9dc5caad91a29e66b7f5417a940cdb5e89eac2f
Bauffman <davy@bauffman.be>
2015-08-26 09:55:23 +0200

    Keep the disabled setting into account.

--- app/controllers/redirects_controller.rb
+++ app/controllers/redirects_controller.rb
@@ -1,7 +1,12 @@
 class RedirectsController < ActionController::Base
   def go
-    redirect = ::Redirect.find_by!(source_uri: source_uri)
-    redirect_to redirect.destination_uri, status: redirect.status_code
+    redirect = ::Redirect.find_by(source_uri: source_uri)
+
+    if redirect && redirect.enabled?
+      redirect_to redirect.destination_uri, status: redirect.status_code
+    else
+      render text: 'No such redirect or disabled.'
+    end
   end
 
   private

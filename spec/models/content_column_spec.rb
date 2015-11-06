587f99f016359b08f2e07f0c8bff914dc7cf90a6
davelens <github@davelens.be>
2015-11-06 15:51:05 +0100

    touch rows when a column gets updated

--- spec/models/content_column_spec.rb
+++ spec/models/content_column_spec.rb
@@ -13,6 +13,15 @@ describe ContentColumn do
     end
   end
 
+  it :touch do
+    column = create(klass)
+    row_update_date = Time.zone.yesterday.to_date
+    column.row.update_attribute(:updated_at, row_update_date)
+    column.width = 11
+    column.save
+    expect(column.row.updated_at.to_date).to eq Date.today
+  end
+
   it '#respond_to?' do
     expect(build(klass)).to respond_to(:row, :content)
   end

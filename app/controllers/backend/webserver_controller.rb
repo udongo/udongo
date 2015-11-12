d61bce5cf859a7dc4852905fb0eed76666007916
davelens <github@davelens.be>
2015-11-12 14:18:26 +0100

    add notice after restart webserver indicating success

--- app/controllers/backend/webserver_controller.rb
+++ app/controllers/backend/webserver_controller.rb
@@ -1,6 +1,6 @@
 class Backend::WebserverController < BackendController
   def restart
     `touch tmp/restart.txt`
-    redirect_to backend_path
+    redirect_to backend_path, notice: t('b.msg.webserver.restarted')
   end
 end

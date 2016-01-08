5bf95ebcb2ba7e3cdecc3e44392e4f9c1005f539
davelens <github@davelens.be>
2016-01-08 11:01:35 +0100

    update schema version

--- spec/dummy/db/schema.rb
+++ spec/dummy/db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151023135358) do
+ActiveRecord::Schema.define(version: 20151205202815) do
 
   create_table "addresses", force: :cascade do |t|
     t.integer  "addressable_id",   limit: 4

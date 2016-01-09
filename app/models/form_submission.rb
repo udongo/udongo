31774ab28968ff81ec95d7be145fc527346ca570
davelens <github@davelens.be>
2016-01-09 20:50:51 +0100

    fix foreign key reference to FormSubmissionData

--- app/models/form_submission.rb
+++ app/models/form_submission.rb
@@ -1,6 +1,6 @@
 class FormSubmission < ActiveRecord::Base
   belongs_to :form
-  has_many :data, class_name: 'FormSubmissionData'
+  has_many :data, foreign_key: :submission_id, class_name: 'FormSubmissionData'
 
   validates :form, presence: true
   # TODO: belongs_to :visitor

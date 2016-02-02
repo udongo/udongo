64a2cf244a29a6f08882a4146defee4275ed5ba5
davelens <github@davelens.be>
2016-02-02 16:46:40 +0100

    add Udongo::EmailVars::FormSubmission

--- lib/udongo/email_vars/form_submission.rb
+++ lib/udongo/email_vars/form_submission.rb
@@ -1,14 +1,18 @@
-class Udongo::EmailVars::FormSubmission
-  attr_reader :submission
+module Udongo
+  module EmailVars
+    class FormSubmission
+      attr_reader :submission
 
-  def initialize(form_submission)
-    @submission = form_submission
-  end
+      def initialize(form_submission)
+        @submission = form_submission
+      end
 
-  def to_hash(prefix: 'submission')
-    submission.data.inject({}) do |hash, current|
-      hash["#{prefix}.#{current.name}"] = current.value
-      hash
+      def to_hash(prefix: 'submission')
+        submission.data.inject({}) do |hash, current|
+          hash["#{prefix}.#{current.name}".to_sym] = current.value
+          hash
+        end
+      end
     end
   end
 end

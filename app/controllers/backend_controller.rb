9c31edc03b42f7abead2b6f2241d9b1d4b1b059e
Bauffman <davy@bauffman.be>
2016-02-04 21:17:08 +0100

    Prefix the admin model request.

--- app/controllers/backend_controller.rb
+++ app/controllers/backend_controller.rb
@@ -8,7 +8,7 @@ class BackendController < ActionController::Base
   helper_method :breadcrumb
 
   def current_admin
-    @current_admin ||= Admin.find(session[:admin_id]) if session[:admin_id]
+    @current_admin ||= ::Admin.find(session[:admin_id]) if session[:admin_id]
   end
   helper_method :current_admin
 

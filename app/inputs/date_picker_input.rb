dfc407ad0a8ac4c8435b528f58fcf046826a6d68
davelens <github@davelens.be>
2016-05-03 11:10:05 +0200

    add app/inputs/date_picker_input.rb

---
+++ app/inputs/date_picker_input.rb
@@ -0,0 +1,29 @@
+class DatePickerInput < SimpleForm::Inputs::Base
+  def input
+    template.content_tag(:div, class: 'input-group date') do
+      template.concat @builder.text_field(attribute_name, input_html_options)
+      template.concat span_table
+    end
+  end
+
+  def data_attributes
+    {
+      date_language: I18n.locale,
+      date_format: 'dd/mm/yyyy'
+    }
+  end
+
+  def icon_table
+    template.content_tag(:span, '', class: 'fa fa-th')
+  end
+
+  def input_html_options
+    super.merge(class: 'form-control', readonly: true, data: data_attributes)
+  end
+
+  def span_table
+    template.content_tag(:span, class: 'input-group-addon') do
+      template.concat icon_table
+    end
+  end
+end

989981b82a2cb1e8014659e3d852b8c953948093
Bauffman <davy@bauffman.be>
2016-05-04 14:01:26 +0200

    Move some code of the link helper to a separate class.

--- app/helpers/link_helper.rb
+++ app/helpers/link_helper.rb
@@ -1,46 +1,33 @@
 module LinkHelper
   def link_to_show(object)
-    str = find_object_path(object)
+    str = Udongo::ObjectPath.find(object)
 
     link_to(
       icon(:search),
-      send(str, *remove_symbols(object)),
+      send(str, *Udongo::ObjectPath.remove_symbols(object)),
       title: t('b.view')
     )
   end
 
   def link_to_edit(object)
-    str = "edit_#{find_object_path(object)}"
+    str = "edit_#{Udongo::ObjectPath.find(object)}"
 
     link_to(
       icon(:pencil_square_o),
-      send(str, *remove_symbols(object)),
+      send(str, *Udongo::ObjectPath.remove_symbols(object)),
       title: t('b.edit')
     )
   end
 
   def link_to_delete(object)
-    str = find_object_path(object)
+    str = Udongo::ObjectPath.find(object)
 
     link_to(
       icon(:trash),
-      send(str, *remove_symbols(object)),
+      send(str, *Udongo::ObjectPath.remove_symbols(object)),
       method: :delete,
       data: { confirm: t('b.msg.confirm') },
       title: t('b.delete')
     )
-  end
-
-  def find_object_path(object)
-    return "#{object.class.name.underscore}_path" unless object.is_a?(Array)
-
-    object.map do |item|
-      item.is_a?(Symbol) ? "#{item}" : "#{item.class.name.underscore}"
-    end.join('_') << '_path'
-  end
-
-  def remove_symbols(object)
-    return object unless object.is_a?(Array)
-    object.select { |o| !o.is_a?(Symbol) }
   end
 end

d2e7a617e82a65821e0c0cf0c7f8115f8313e5a5
davelens <github@davelens.be>
2016-05-09 15:05:42 +0200

    add Tether and load it prior to Bootstrap in backend

--- app/assets/javascripts/backend/application.js
+++ app/assets/javascripts/backend/application.js
@@ -14,6 +14,7 @@
 //= require jquery_ujs
 //= require jquery-ui/autocomplete
 //= require jquery-ui/sortable
+//= require backend/tether.min
 //= require backend/bootstrap
 //= require ckeditor/init
 //= require backend/general

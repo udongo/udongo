2e340c116b8492153420f3913a80cb080d782f8e
davelens <github@davelens.be>
2016-05-09 11:23:26 +0200

    rename DatePickerInput to DatepickerInput to preserve sane class names on the element

--- app/inputs/datepicker_input.rb
+++ app/inputs/datepicker_input.rb
@@ -1,4 +1,4 @@
-class DatePickerInput < SimpleForm::Inputs::Base
+class DatepickerInput < SimpleForm::Inputs::Base
   def input
     template.content_tag(:div, class: 'input-group') do
       template.concat @builder.text_field(attribute_name, input_html_options)

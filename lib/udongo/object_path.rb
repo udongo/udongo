b6d660b0e2b8ed57add42a3325d6911d9e11638d
Bauffman <davy@bauffman.be>
2016-05-18 11:38:46 +0200

    Refactor the object path class.

--- lib/udongo/object_path.rb
+++ lib/udongo/object_path.rb
@@ -1,16 +1,22 @@
 class Udongo::ObjectPath
   def self.find(object)
     unless object.is_a?(Array)
-      return "#{object.class.name.underscore}_path".gsub('_decorator', '')
+      return cleanup("#{object.class.name.underscore}_path")
     end
 
     object.map do |item|
-      item.is_a?(Symbol) ? "#{item}" : "#{item.class.name.underscore}".gsub('_decorator', '')
+      item.is_a?(Symbol) ? "#{item}" : cleanup(item.class.name.underscore)
     end.join('_') << '_path'
   end
 
   def self.remove_symbols(object)
     return object unless object.is_a?(Array)
     object.select { |o| !o.is_a?(Symbol) }
+  end
+
+  private
+
+  def self.cleanup(value)
+    value.to_s.gsub('_decorator', '')
   end
 end

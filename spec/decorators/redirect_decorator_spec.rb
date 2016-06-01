c6eb7447e73d895d70c4c3cc0edccc2d5301e2f6
Bauffman <davy@bauffman.be>
2016-06-01 15:03:33 +0200

    Add better specs for the redirect decorator.

--- spec/decorators/redirect_decorator_spec.rb
+++ spec/decorators/redirect_decorator_spec.rb
@@ -1,9 +1,17 @@
 require 'rails_helper'
 
 describe RedirectDecorator do
+  let(:instance) { build(:redirect).decorate }
+
+  it '#status_code_collection' do
+    expect(instance.status_code_collection).to eq [
+                                                    ['301 (Moved Permanently)', '301'],
+                                                    ['303 (See Other)', '303'],
+                                                    ['307 (Temporary Redirect)', '307']
+                                                  ]
+  end
+
   it '#respond_to?' do
-    expect(build(:redirect).decorate).to respond_to(
-      :status_code_collection, :summary
-    )
+    expect(instance).to respond_to(:status_code_collection, :summary)
   end
 end

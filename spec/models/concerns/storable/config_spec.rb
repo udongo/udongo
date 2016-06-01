e0d84a4dcc8819919aa032ae4c67dfa9d43a51d0
Bauffman <davy@bauffman.be>
2016-06-01 14:14:44 +0200

    Fix the storable config spec.

--- spec/models/concerns/storable/config_spec.rb
+++ spec/models/concerns/storable/config_spec.rb
@@ -4,42 +4,29 @@ describe Concerns::Storable::Config do
   let(:klass) { described_class }
   let(:config) { Concerns::Storable::Config.new }
 
-  describe '#fields' do
-    it :default do
-      expect(config.fields).to eq({})
-    end
+  it '#fields' do
+    expect(config.fields).to eq({})
   end
 
   describe '#add' do
-    it 'as intended' do
-      config.add(:first_name, :string)
-      expect(config.fields).to eq({
-        first_name: { klass: :string, default: nil }
+    it 'without default value' do
+      config.add :first_name, String
+      expect(config.fields).to eq ({
+        first_name: { type: String, default: nil }
       })
     end
 
-    it 'as strings' do
-      config.add('first_name', 'string', '')
-      expect(config.fields).to eq({
-        first_name: { klass: :string, default: ''}
+    it 'with default value' do
+      config.add :first_name, String, 'foo'
+      expect(config.fields).to eq ({
+        first_name: { type: String, default: 'foo' }
       })
-    end
-
-    it 'only allow certain klasses' do
-      klass::KLASSES.keys.each do |k|
-        config.add(:foo, k)
-        expect(config.fields[:foo][:klass]).to eq k.to_sym
-      end
-
-      %w(foo bar baz).each do |k |
-        expect { config.add(:foo, k) }.to raise_error("#{k} is not a valid storable config klass")
-      end
     end
   end
 
   describe '#allowed?' do
     it :true do
-      config.add(:foo, :string)
+      config.add :foo, String
       expect(config).to be_allowed('foo')
     end
 

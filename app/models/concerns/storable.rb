ed5207941d4da86d39b7aee1526d2665f59f8f01
Bauffman <davy@bauffman.be>
2016-06-01 14:37:54 +0200

    Fix the storable collection class.

--- app/models/concerns/storable.rb
+++ app/models/concerns/storable.rb
@@ -19,35 +19,21 @@ module Concerns
     def store(collection = :default)
       @store_collections = {} unless @store_collections
 
-      collection = collection.to_sym
-
-      unless @store_collections[collection]
-        @store_collections[collection] = Concerns::Storable::Collection.new(
-          self, collection
+      unless @store_collections[collection.to_sym]
+        @store_collections[collection.to_sym] = Concerns::Storable::Collection.new(
+          self, collection, self.class.store_config
         )
-
-        @store_collections[collection].extend(Virtus.model)
-
-        # create all attributes
-        self.class.store_config.fields.each do |field,options|
-          @store_collections[collection].attribute field, options[:type], default: options[:default], lazy: true
-        end
-
-        # set all values
-        stores.where(collection: collection).pluck(:name, :value).each do |field,value|
-          if self.class.store_config.allowed?(field)
-            @store_collections[collection].send("#{field}=", value)
-          end
-        end
       end
 
-      @store_collections[collection]
+      @store_collections[collection.to_sym]
     end
 
     module ClassMethods
       def storable_field(name, type, default = nil)
+        # TODO can be oneliner
         delegate name, to: :store
         delegate "#{name}=", to: :store
+
         store_config.add name, type, default
       end
 
--- app/models/concerns/storable/collection.rb
+++ app/models/concerns/storable/collection.rb
@@ -1,9 +1,11 @@
 module Concerns
   module Storable
     class Collection
-      def initialize(parent, category)
+      def initialize(parent, category, config)
         @parent = parent
         @category = category
+
+        init_attributes(config)
       end
 
       def save
@@ -17,6 +19,24 @@ module Concerns
 
           tmp.value = value
           tmp.save!
+        end
+      end
+
+      private
+
+      def init_attributes(config)
+        extend(Virtus.model)
+
+        config.fields.each do |field,options|
+          attribute field, options[:type], default: options[:default], lazy: true
+        end
+
+        ::Store.where(
+          storable_type: @parent.class,
+          storable_id: @parent.id,
+          collection: @category,
+        ).pluck(:name, :value).each do |field, value|
+          send "#{field}=", value if config.allowed?(field)
         end
       end
     end

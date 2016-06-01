e869dbdf75ce7b2c8ac5404ac5d2b23a174b172e
Bauffman <davy@bauffman.be>
2016-06-01 14:03:11 +0200

    Get rid of the store klass when saving a collection.

--- app/models/concerns/storable/collection.rb
+++ app/models/concerns/storable/collection.rb
@@ -6,14 +6,12 @@ module Concerns
         @category = category
       end
 
-      # TODO the 'klass' field is not required in the db.
       def save
         attributes.each do |name,value|
           tmp = ::Store.find_or_initialize_by(
             collection: @category,
             storable_type: @parent.class.name,
             storable_id: @parent.id,
-            klass: 'FooBar',
             name: name
           )
 

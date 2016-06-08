4e03e52b5a9e2a4b0abdd8256a87c5601d326023
Bauffman <davy@bauffman.be>
2016-06-08 11:49:24 +0200

    Use tr instead of gsub.

--- app/helpers/icon_helper.rb
+++ app/helpers/icon_helper.rb
@@ -1,6 +1,6 @@
 module IconHelper
   def icon(name, label = nil)
-    markup = content_tag :i, nil, class: "fa fa-#{name.to_s.gsub('_', '-')}"
+    markup = content_tag :i, nil, class: "fa fa-#{name.to_s.tr('_', '-')}"
     label ? "#{markup}&nbsp;#{label}".html_safe : markup
   end
 end

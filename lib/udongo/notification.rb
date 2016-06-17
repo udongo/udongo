78b3cdeef8f4f29c469e843a3c595d3489d8e065
davelens <github@davelens.be>
2016-06-17 12:05:31 +0200

    add a way to add an options hash to Udongo::Notification#translate_notice

--- lib/udongo/notification.rb
+++ lib/udongo/notification.rb
@@ -1,5 +1,11 @@
 class Udongo::Notification
-  def translate_notice(notice, actor)
-    I18n.t("b.msg.#{notice}", actor: I18n.t("b.#{actor}"))
+  def translate_notice(notice, *args)
+    return I18n.t("b.msg.#{notice}") if args.blank?
+
+    if args.first.is_a?(Hash)
+      I18n.t("b.msg.#{notice}", args.first)
+    else
+      I18n.t("b.msg.#{notice}", actor: I18n.t("b.#{args.first}"))
+    end
   end
 end
--- spec/lib/udongo/notification_spec.rb
+++ spec/lib/udongo/notification_spec.rb
@@ -7,10 +7,20 @@ describe Udongo::Notification do
   end
 
   describe '#translate_notice' do
-    it 'with 1 actor' do
+    it 'without params' do
+      allow(I18n).to receive(:t).with('b.msg.foobar') { 'Dit is een melding zonder vars' }
+      expect(instance.translate_notice(:foobar)).to eq 'Dit is een melding zonder vars'
+    end
+
+    it '"old" syntax with 1 actor' do
       allow(I18n).to receive(:t).with('b.foo') { 'Foo' }
       allow(I18n).to receive(:t).with("b.msg.added", actor: 'Foo') { 'Foo werd toegevoegd' }
       expect(instance.translate_notice(:added, :foo)).to eq 'Foo werd toegevoegd'
+    end
+
+    it 'multiple vars' do
+      allow(I18n).to receive(:t).with("b.msg.added", foo: 'bar', total: 30) { '30 foos in bar' }
+      expect(instance.translate_notice(:added, foo: 'bar', total: 30)).to eq '30 foos in bar'
     end
   end
 

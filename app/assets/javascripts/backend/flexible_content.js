535dcd6ae77e05fff94df91a29b642a5fe11f5af
davelens <github@davelens.be>
2016-06-29 18:34:34 +0200

    remove flexible_content.update_position_listener alltogether in favor of updated sortable.update_position_listener

--- app/assets/javascripts/backend/flexible_content.js
+++ app/assets/javascripts/backend/flexible_content.js
@@ -3,24 +3,14 @@ var flexible_content = flexible_content || {
     $('#content-rows').sortable({
       axis: 'y',
       handle: '.card-header',
-      update: flexible_content.update_position_listener
+      update: sortable.update_position_listener
     });
 
     $('.content-columns').sortable({
       axis: 'x',
       handle: '.card-header',
-      update: flexible_content.update_position_listener
+      update: sortable.update_position_listener
     })
-  },
-
-  update_position_listener: function(e, ui) {
-    var new_position = ui.item.index() + 1
-
-    $.ajax({
-      data: { position: new_position },
-      url: ui.item.data('update-position'),
-      type: 'POST'
-    });
   }
 }
 

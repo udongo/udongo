c99fd27e6d42e97c3d576c362892dfe92fa28cbb
davelens <github@davelens.be>
2016-06-29 18:32:43 +0200

    realise we only need 1 update listener

--- app/assets/javascripts/backend/flexible_content.js
+++ app/assets/javascripts/backend/flexible_content.js
@@ -3,27 +3,17 @@ var flexible_content = flexible_content || {
     $('#content-rows').sortable({
       axis: 'y',
       handle: '.card-header',
-      update: flexible_content.update_row_position_listener
+      update: flexible_content.update_position_listener
     });
 
     $('.content-columns').sortable({
       axis: 'x',
       handle: '.card-header',
-      update: flexible_content.update_column_position_listener
+      update: flexible_content.update_position_listener
     })
   },
 
-  update_column_position_listener: function(e, ui) {
-    var new_position = ui.item.index() + 1
-
-    $.ajax({
-      data: { position: new_position },
-      url: ui.item.data('update-position'),
-      type: 'POST'
-    });
-  },
-
-  update_row_position_listener: function(e, ui) {
+  update_position_listener: function(e, ui) {
     var new_position = ui.item.index() + 1
 
     $.ajax({

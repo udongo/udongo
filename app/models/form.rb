a18dc6839e9f35b4b35b760e28d26d9c22edb6c8
davelens <github@davelens.be>
2016-07-02 18:10:43 +0200

    add Form#data relation

--- app/models/form.rb
+++ app/models/form.rb
@@ -4,6 +4,7 @@ class Form < ActiveRecord::Base
 
   has_many :fields, class_name: 'FormField', dependent: :destroy
   has_many :submissions, class_name: 'FormSubmission', dependent: :destroy
+  has_many :data, through: :submissions
 
   validates :identifier, presence: true
 end

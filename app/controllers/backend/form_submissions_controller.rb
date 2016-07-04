a98e5222acefe20e30c7f12f92e8b60f16d49f0f
davelens <github@davelens.be>
2016-07-04 11:02:54 +0200

    refactor to use data_object in view

--- app/controllers/backend/form_submissions_controller.rb
+++ app/controllers/backend/form_submissions_controller.rb
@@ -1,10 +1,6 @@
 class Backend::FormSubmissionsController < BackendController
   before_action :find_form
 
-  def index
-    @submissions = @form.submissions.map(&:data_object)
-  end
-
   private
 
   def find_form

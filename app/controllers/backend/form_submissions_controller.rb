5826afbe6f412cb095130de942c93cf6eac1da5c
davelens <github@davelens.be>
2016-07-11 14:59:22 +0200

    define separate instance var for form submissions

--- app/controllers/backend/form_submissions_controller.rb
+++ app/controllers/backend/form_submissions_controller.rb
@@ -1,6 +1,10 @@
 class Backend::FormSubmissionsController < BackendController
   before_action :find_form
 
+  def index
+    @submissions = FormSubmission.all.decorate
+  end
+
   private
 
   def find_form

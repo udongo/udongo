580822ba9218092673ca3f1444c363d5fc4156e7
davelens <github@davelens.be>
2016-07-12 12:27:36 +0200

    add placeholder for draggable flexible content rows

--- app/assets/javascripts/backend/flexible_content.js
+++ app/assets/javascripts/backend/flexible_content.js
@@ -2,9 +2,14 @@ var flexible_content = flexible_content || {
   init: function() {
     $('#content-rows').sortable({
       axis: 'y',
-      tolerance: 'pointer',
       handle: '.card-header',
-      update: sortable.update_position_listener
+      update: sortable.update_position_listener,
+      placeholder: 'row-placeholder',
+      tolerance: 'pointer',
+      start: function(e, ui){
+        ui.placeholder.height(ui.item.height());
+        ui.placeholder.css('margin-bottom', ui.item.css('margin-bottom'));
+      }
     });
 
     $('.content-columns').sortable({

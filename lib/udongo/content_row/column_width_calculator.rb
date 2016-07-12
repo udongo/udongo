35d1cdf10fc1be74eab9c4235cb4474fe6d0ae52
davelens <github@davelens.be>
2016-07-12 17:32:01 +0200

    add Udongo::ContentRow::ColumnWidthCalculator to help determine how big new columns should be

---
+++ lib/udongo/content_row/column_width_calculator.rb
@@ -0,0 +1,32 @@
+module Udongo
+  module ContentRow
+    class ColumnWidthCalculator
+      attr_reader :row
+
+      COLUMNS = %w(width_xs width_sm width_md width_lg width_xl)
+
+      def initialize(row)
+        @row = row
+      end
+
+      def calculate(field)
+        difference = 12 - total(field)
+        return 12 if difference.zero?
+        difference
+      end
+
+      def hashed_values
+        COLUMNS.inject({}) do |result, column|
+          result[column.to_sym] = calculate(column.to_sym)
+          result
+        end
+      end
+
+      def total(field)
+        sum = row.columns.sum(field.to_sym).to_i
+        return 12 if sum > 12
+        sum
+      end
+    end
+  end
+end

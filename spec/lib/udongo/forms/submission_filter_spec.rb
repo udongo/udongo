b192cb06c48e4d6ec2ee553ea6fc284df2824dd6
davelens <github@davelens.be>
2016-07-16 16:55:48 +0200

    expand Udongo::Forms::SubmissionFilter spec

--- spec/lib/udongo/forms/submission_filter_spec.rb
+++ spec/lib/udongo/forms/submission_filter_spec.rb
@@ -5,14 +5,32 @@ describe Udongo::Forms::SubmissionFilter do
   let(:instance) { described_class.new(form) }
 
   describe '#result' do
+    let(:a) { create(:form_submission) }
+    let(:b) { create(:form_submission) }
+
     it 'default' do
       expect(instance.result).to eq []
     end
 
-    it 'without filter' do
-      submission = create(:form_submission)
-      create(:form_submission_data, submission: submission, name: 'foo', value: 'bar')
-      expect(instance.result).to eq [submission]
+    context 'data present' do
+      before(:each) do
+        create(:form_submission_data, submission: a, name: 'foo', value: 'bar')
+        create(:form_submission_data, submission: b, name: 'foo', value: 'baz')
+      end
+
+      it 'no params given, ignore filter' do
+        expect(instance.result).to eq [a, b]
+      end
+
+      it 'params given, no results' do
+        allow(instance).to receive(:params) { { foo: 'blub' } }
+        expect(instance.result).to eq []
+      end
+
+      it 'params given, with result' do
+        allow(instance).to receive(:params) { { foo: 'baz' } }
+        expect(instance.result).to eq [b]
+      end
     end
   end
 
@@ -21,6 +39,6 @@ describe Udongo::Forms::SubmissionFilter do
   end
 
   it '#responds_to?' do
-    expect(instance).to respond_to(:result)
+    expect(instance).to respond_to(:result, :config, :fields)
   end
 end

9629be725c2f0bf61d858d121e9add0e28ea7744
davelens <github@davelens.be>
2016-07-22 15:09:29 +0200

    alter FormDecorator#email_present? to return false when a specific datagrid_fields config is present for the form

--- app/decorators/form_decorator.rb
+++ app/decorators/form_decorator.rb
@@ -5,7 +5,14 @@ class FormDecorator < ApplicationDecorator
     @datagrid ||= Udongo::Forms::SubmissionDatagrid.new(object)
   end
 
+  # TODO: See if we can move this to Udongo::Forms::SubmissionDatagrid
+  # after Davy's config overhaul.
+  def datagrid_fields_configured?
+    Udongo.config.forms.send(identifier).datagrid_fields.any?
+  end
+
   def email_present?
-    Form.first.data.select('DISTINCT name').map(&:name).include?('email')
+    return false if datagrid_fields_configured?
+    data.select('DISTINCT name').map(&:name).include?('email')
   end
 end
--- spec/decorators/form_decorator_spec.rb
+++ spec/decorators/form_decorator_spec.rb
@@ -1,15 +1,31 @@
 require 'rails_helper'
 
 describe FormDecorator do
-  let(:instance) { create(:form).decorate }
+  let(:form) { create(:form, identifier: 'foo') }
+  let(:instance) { form.decorate }
   it('decorated?') { expect(instance).to be_decorated_with described_class }
 
   it '#datagrid' do
     expect(instance.datagrid).to be_instance_of Udongo::Forms::SubmissionDatagrid
   end
 
+  describe '#datagrid_fields_configured?' do
+    it 'true' do
+      allow(Udongo.config.forms).to receive(:foo) { OpenStruct.new(datagrid_fields: %w(email)) }
+      expect(instance.datagrid_fields_configured?).to be true
+    end
+
+    it 'false' do
+      expect(instance.datagrid_fields_configured?).to be false
+    end
+  end
+
   describe '#email_present?' do
-    let(:submission) { create(:form_submission) }
+    let(:submission) { create(:form_submission, form: form) }
+
+    before(:each) do
+      allow(instance).to receive(:datagrid_fields_configured?) { false }
+    end
 
     it 'true' do
       create(:form_submission_data, submission: submission, name: 'email')
@@ -17,16 +33,23 @@ describe FormDecorator do
       expect(instance.email_present?).to be true
     end
 
-    it 'false' do
-      create(:form_submission_data, submission: submission, name: 'foo')
-      allow(instance.object).to receive(:submissions) { [submission] }
-      expect(instance.email_present?).to be false
+    describe 'false' do
+      it 'no email column in submission data' do
+        create(:form_submission_data, submission: submission, name: 'foo')
+        allow(instance.object).to receive(:submissions) { [submission] }
+        expect(instance.email_present?).to be false
+      end
+
+      it 'specific datagrid_fields config present' do
+        allow(instance).to receive(:datagrid_fields_configured?) { true }
+        expect(instance.email_present?).to be false
+      end
     end
   end
 
   it '#responds_to?' do
     expect(instance).to respond_to(
-      :datagrid, :email_present?
+      :datagrid, :datagrid_fields_configured?, :email_present?
     )
   end
 end

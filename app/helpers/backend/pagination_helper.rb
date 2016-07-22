55c3aa687bf8549300c755c1ee5f4e0249f9d19f
davelens <github@davelens.be>
2016-07-22 16:15:00 +0200

    refactor defaults from Backend::PaginationHelper#udongo_paginate to get filtered through Udongo::WillPaginate::Options

--- app/helpers/backend/pagination_helper.rb
+++ app/helpers/backend/pagination_helper.rb
@@ -1,17 +1,7 @@
 module Backend
   module PaginationHelper
-
     def udongo_paginate(collection, options = {})
-      options.merge!(
-        class: 'pagination',
-        inner_window: 1,
-        outer_window: 0,
-        renderer: Udongo::WillPaginate::Renderer,
-        previous_label: '&lt;'.html_safe,
-        next_label: '&gt;'.html_safe
-      )
-
-      will_paginate collection, options
+      will_paginate(collection, Udongo::WillPaginate::Options.new(options).values)
     end
   end
 end

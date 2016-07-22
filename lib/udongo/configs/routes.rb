2ffab62604dafe89231f91e05af82c50a5b2f0bc
Bauffman <davy@bauffman.be>
2016-07-22 16:14:08 +0200

    Add helper method for the prefix_with_locale? setting.

--- lib/udongo/configs/routes.rb
+++ lib/udongo/configs/routes.rb
@@ -4,6 +4,10 @@ module Udongo
       include Virtus.model
 
       attribute :prefix_with_locale, Axiom::Types::Boolean, default: true
+
+      def prefix_with_locale?
+        prefix_with_locale === true
+      end
     end
   end
 end
--- spec/lib/udongo/configs/routes_spec.rb
+++ spec/lib/udongo/configs/routes_spec.rb
@@ -2,6 +2,7 @@ require 'rails_helper'
 
 describe Udongo::Configs::Routes do
   let(:klass) { described_class }
+  let(:instance) { klass.new }
 
   describe 'defaults' do
     it :prefix_with_locale do
@@ -9,7 +10,21 @@ describe Udongo::Configs::Routes do
     end
   end
 
+  describe '#prefix_with_locale' do
+    it :true do
+      instance.prefix_with_locale = true
+      expect(instance).to be_prefix_with_locale
+    end
+
+    it :false do
+      instance.prefix_with_locale = false
+      expect(instance).not_to be_prefix_with_locale
+    end
+  end
+
   it '#respond_to?' do
-    expect(klass.new).to respond_to(:prefix_with_locale, :prefix_with_locale=)
+    expect(klass.new).to respond_to(
+      :prefix_with_locale, :prefix_with_locale=, :prefix_with_locale?
+    )
   end
 end

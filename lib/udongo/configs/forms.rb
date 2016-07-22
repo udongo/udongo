fd0be953c3b65863f6d7f2f8c9d1e60677c9fa52
davelens <github@davelens.be>
2016-07-22 13:50:59 +0200

    make sure form configs do not get overridden

--- lib/udongo/configs/forms.rb
+++ lib/udongo/configs/forms.rb
@@ -7,7 +7,7 @@ module Udongo
 
       def method_missing(method, *args, &block)
         form = ::Form.find_by!(identifier: method)
-        @submission_datagrids[form.identifier.to_sym] = OpenStruct.new(datagrid_fields: [], filter_fields: [])
+        @submission_datagrids[form.identifier.to_sym] ||= OpenStruct.new(datagrid_fields: [], filter_fields: [])
       end
     end
   end

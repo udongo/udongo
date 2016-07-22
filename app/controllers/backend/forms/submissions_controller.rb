a321bef94a7b9b30ccaf1cc18240d82e64210896
Bauffman <davy@bauffman.be>
2016-07-22 13:48:02 +0200

    Add form breadcrumb navigation.

--- app/controllers/backend/forms/submissions_controller.rb
+++ app/controllers/backend/forms/submissions_controller.rb
@@ -1,15 +1,8 @@
 class Backend::Forms::SubmissionsController < Backend::Forms::BaseController
   include Concerns::PaginationController
-  before_action :find_form
 
   def index
     @filter = Udongo::Forms::SubmissionFilter.search(@form, params[:q])
     @submissions = paginate(@filter.result).decorate
-  end
-
-  private
-
-  def find_form
-    @form ||= Form.find(params[:form_id].to_i).decorate
   end
 end
--- app/controllers/backend/forms_controller.rb
+++ app/controllers/backend/forms_controller.rb
@@ -1,4 +1,6 @@
 class Backend::FormsController < BackendController
+  before_action -> { breadcrumb.add t('b.forms'), backend_forms_path }
+
   def index
     @forms = Form.all
   end

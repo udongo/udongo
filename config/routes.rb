65c7cefc5393e45230c3e12c10620c7d0d289670
Bauffman <davy@bauffman.be>
2016-07-22 16:06:50 +0200

    Refer to the correct flexible content types config setting.

--- config/routes.rb
+++ config/routes.rb
@@ -67,7 +67,7 @@ Rails.application.routes.draw do
       end
 
       scope module: 'rows' do
-        Udongo.config.flexible_content_types.each do |content_type|
+        Udongo.config.flexible_content.types.each do |content_type|
           resources content_type.to_s.pluralize.to_sym, only: [:edit, :update]
         end
       end

49055fcfa5de2eb3ac5a8b45f06b689891513e76
davelens <github@davelens.be>
2016-07-22 13:32:58 +0200

    let Udongo::Configs::Forms#method_missing do a hard check for form existence

--- lib/udongo/configs/forms.rb
+++ lib/udongo/configs/forms.rb
@@ -6,7 +6,7 @@ module Udongo
       end
 
       def method_missing(method, *args, &block)
-        form = ::Form.find_by(identifier: method)
+        form = ::Form.find_by!(identifier: method)
         @submission_datagrids[form.identifier.to_sym] = OpenStruct.new(datagrid_fields: [], filter_fields: [])
       end
     end

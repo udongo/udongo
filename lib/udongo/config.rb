183376f2aa2831ec7e2c6d06bb05a7011ec6c2a2
Bauffman <davy@bauffman.be>
2016-07-22 15:32:42 +0200

    Use method_missing to remove useless code.

--- lib/udongo/config.rb
+++ lib/udongo/config.rb
@@ -1,12 +1,12 @@
 module Udongo
   class Config
-    attr_reader :i18n
-
     attr_accessor :default_locale, :locales, :prefix_routes_with_locale, :host,
                   :time_zone, :allow_new_tags, :flexible_content_types,
                   :project_name
 
     def initialize
+      @configs = {}
+
       @default_locale = :nl
       @locales = %w(nl en fr de)
       @prefix_routes_with_locale = true
@@ -15,8 +15,16 @@ module Udongo
       @allow_new_tags = true
       @flexible_content_types = %w(text image)
       @project_name = 'Udongo'
+    end
 
-      @i18n ||= Udongo::Configs::I18n.new
+    def method_missing(method_name, *arguments, &block)
+      super if method_name.to_s.include?('=')
+
+      unless @configs[method_name]
+        @configs[method_name] = "Udongo::Configs::#{method_name.to_s.camelcase}".constantize.new
+      end
+
+      @configs[method_name]
     end
 
     def prefix_routes_with_locale?

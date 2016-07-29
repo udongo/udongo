b00185baa6849e5e5e89b4454d783a064737c1cf
Bauffman <davy@bauffman.be>
2016-07-29 16:04:20 +0200

    Add spec for the snippet translation form.

--- spec/forms/backend/snippet_translation_form_spec.rb
+++ spec/forms/backend/snippet_translation_form_spec.rb
@@ -1,82 +1,29 @@
 require 'rails_helper'
 
 describe Backend::SnippetTranslationForm do
-  # let(:klass) { described_class }
-  # let(:valid_params) do
-  #   {
-  #     identifier: 'foo',
-  #     description: 'bar'
-  #   }
-  # end
-  # let(:instance) { klass.new }
-  #
-  # describe 'validations' do
-  #   describe 'presence' do
-  #     it :description do
-  #       params = valid_params.merge(description: nil)
-  #       expect(instance.save(params)).to eq false
-  #     end
-  #   end
-  #
-  #   describe 'identifier' do
-  #     it :presence do
-  #       params = valid_params.merge(identifier: nil)
-  #       expect(instance.save(params)).to eq false
-  #     end
-  #
-  #     describe 'unique' do
-  #       it 'create' do
-  #         create(:snippet, valid_params)
-  #
-  #         params = valid_params.merge(identifier: 'foo')
-  #         expect(instance.save(params)).to eq false
-  #       end
-  #
-  #       it 'update' do
-  #         snippet = create(:snippet, valid_params)
-  #
-  #         params = valid_params.merge(identifier: 'foo')
-  #         expect(klass.new(snippet).save(params)).to eq true
-  #       end
-  #     end
-  #   end
-  # end
-  #
-  # describe '#save' do
-  #   it 'create' do
-  #     expect(instance.save(valid_params)).to eq true
-  #
-  #     snippet = Snippet.first
-  #     expect(snippet.identifier).to eq 'foo'
-  #     expect(snippet.description).to eq 'bar'
-  #   end
-  #
-  #   it 'update' do
-  #     snippet = create(:snippet, valid_params)
-  #
-  #     expect(klass.new(snippet).save(
-  #       identifier: 'bar',
-  #       description: 'baz'
-  #     )).to eq true
-  #
-  #     snippet = Snippet.first
-  #     expect(snippet.identifier).to eq 'bar'
-  #     expect(snippet.description).to eq 'baz'
-  #   end
-  # end
-  #
-  # describe '#persisted?' do
-  #   it :true do
-  #     snippet = create(:snippet)
-  #     expect(klass.new(snippet)).to be_persisted
-  #   end
-  #
-  #   it :false do
-  #     expect(klass.new).not_to be_persisted
-  #   end
-  # end
-  #
-  # it '#respond_to' do
-  #   expect(klass.new).to respond_to(:save, :persisted?, :snippet)
-  # end
+  let(:klass) { described_class }
+
+  it '#save' do
+    snippet = create(:snippet)
+
+    expect(klass.new(snippet, snippet.translation(:nl)).save(
+      title: 'bar',
+      content: 'foo'
+    )).to eq true
+
+    translation = Snippet.first.translation(:nl)
+    expect(translation.title).to eq 'bar'
+    expect(translation.content).to eq 'foo'
+  end
+
+  it '#persisted?' do
+    snippet = create(:snippet)
+    expect(klass.new(snippet, snippet.translation)).to be_persisted
+  end
+
+  it '#respond_to' do
+    snippet = create(:snippet)
+    instance = klass.new(snippet, snippet.translation)
+    expect(instance).to respond_to(:save, :persisted?, :snippet)
+  end
 end

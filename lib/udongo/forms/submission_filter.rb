1b0f73bb7493879626cf6a6b8ffc005b54a98f17
davelens <github@davelens.be>
2016-08-01 15:18:22 +0200

    fix form submission filter to search data using the Rails 5 ActiveRelation#or method

--- lib/udongo/forms/submission_filter.rb
+++ lib/udongo/forms/submission_filter.rb
@@ -8,6 +8,10 @@ module Udongo
         @params = params || {}
       end
 
+      def condition(key, value)
+        @form.data.where(name: key).where('value REGEXP ?', value)
+      end
+
       def fields
         Udongo.config.forms.send(@form.identifier).filter_fields
       end
@@ -15,9 +19,16 @@ module Udongo
       def result
         data = @form.data
 
-        params.each do |key,value|
+        params.inject([]) do |conditions,param|
+          key, value = param
           next if value.blank?
-          data = data.where(name: key).where('value REGEXP ?', value)
+
+          if conditions.any?
+            data = data.or(condition(key, value))
+          else
+            data = condition(key, value)
+            conditions << data
+          end
         end
 
         @form.submissions.where(id: data.pluck(:submission_id).uniq)

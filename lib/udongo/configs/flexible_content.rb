5334724415ccbc9dd454c05855b9121d428528cd
davelens <github@davelens.be>
2016-08-05 15:49:37 +0200

    rename "width" to "dimension" in flexible content columns

--- lib/udongo/configs/flexible_content.rb
+++ lib/udongo/configs/flexible_content.rb
@@ -4,10 +4,10 @@ module Udongo
       include Virtus.model
 
       attribute :types, Array, default: %w(text image)
-      attribute :allowed_column_widths, Array, default: %w(xs sm md lg xl)
+      attribute :allowed_column_dimensions, Array, default: %w(xs sm md lg xl)
 
-      def column_width_allowed?(dimension)
-        allowed_column_widths.include?(dimension.to_s)
+      def column_dimension_allowed?(value)
+        allowed_column_dimensions.include?(value.to_s)
       end
     end
   end
--- spec/lib/udongo/configs/flexible_content_spec.rb
+++ spec/lib/udongo/configs/flexible_content_spec.rb
@@ -4,8 +4,8 @@ describe Udongo::Configs::FlexibleContent do
   let(:instance) { described_class.new }
 
   describe 'defaults' do
-    it :allowed_column_widths do
-      expect(instance.allowed_column_widths).to eq %w(xs sm md lg xl)
+    it :allowed_column_dimensions do
+      expect(instance.allowed_column_dimensions).to eq %w(xs sm md lg xl)
     end
 
     it :types do
@@ -13,22 +13,22 @@ describe Udongo::Configs::FlexibleContent do
     end
   end
 
-  describe '#column_width_allowed?' do
+  describe '#column_dimension_allowed?' do
     it 'true' do
-      allow(instance).to receive(:allowed_column_widths) { %w(xs sm md lg xl) }
-      expect(instance.column_width_allowed?(:sm)).to be true
+      allow(instance).to receive(:allowed_column_dimensions) { %w(xs sm md lg xl) }
+      expect(instance.column_dimension_allowed?(:sm)).to be true
     end
 
     it 'false' do
-      allow(instance).to receive(:allowed_column_widths) { %w(xs) }
-      expect(instance.column_width_allowed?(:sm)).to be false
+      allow(instance).to receive(:allowed_column_dimensions) { %w(xs) }
+      expect(instance.column_dimension_allowed?(:sm)).to be false
     end
   end
 
   it '#respond_to?' do
     expect(instance).to respond_to(
-      :types, :types=, :allowed_column_widths, :allowed_column_widths=,
-      :column_width_allowed?
+      :types, :types=, :allowed_column_dimensions, :allowed_column_dimensions=,
+      :column_dimension_allowed?
     )
   end
 end

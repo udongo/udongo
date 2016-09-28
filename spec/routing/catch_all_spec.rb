20586723a76c957227c18db9ab1ce10cf5b8dc05
Bauffman <davy@bauffman.be>
2016-09-28 20:34:42 +0200

    Add route specs for catch all.

---
+++ spec/routing/catch_all_spec.rb
@@ -0,0 +1,27 @@
+require 'rails_helper'
+
+describe 'catch all' do
+  it 'GET /foo' do
+    expect(get: '/foo').to route_to(
+                             controller: 'catch_all',
+                             action: 'resolve',
+                             path: 'foo'
+                           )
+  end
+
+  it 'GET /foo/bar' do
+    expect(get: '/foo/bar').to route_to(
+                                 controller: 'catch_all',
+                                 action: 'resolve',
+                                 path: 'foo/bar'
+                               )
+  end
+
+  it 'GET /foo/bar/baz' do
+    expect(get: '/foo/bar/baz').to route_to(
+                                     controller: 'catch_all',
+                                     action: 'resolve',
+                                     path: 'foo/bar/baz'
+                                   )
+  end
+end

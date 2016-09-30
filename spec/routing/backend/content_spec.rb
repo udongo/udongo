d49e8723aebd177161eec4afecb49521523a9e5b
Bauffman <davy@bauffman.be>
2016-09-30 15:56:56 +0200

    Prepare for the content routes.

---
+++ spec/routing/backend/content_spec.rb
@@ -0,0 +1,21 @@
+require 'rails_helper'
+
+describe 'content routes' do
+  # namespace :content do
+  #   resources :rows, only: [:index, :new, :destroy] do
+  #     concerns :positionable
+  #
+  #     scope module: 'rows' do
+  #       resources :columns do
+  #         concerns :positionable
+  #       end
+  #     end
+  #   end
+  #
+  #   scope module: 'rows' do
+  #     Udongo.config.flexible_content.types.each do |content_type|
+  #       resources content_type.to_s.pluralize.to_sym, only: [:edit, :update]
+  #     end
+  #   end
+  # end
+end

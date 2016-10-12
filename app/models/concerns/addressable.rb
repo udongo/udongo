8a74ee5f08f43babbe64bf1fcaf436e4237f7b23
Bauffman <davy@bauffman.be>
2016-10-12 07:46:21 +0200

    Add some placeholder code for the address concern.

--- app/models/concerns/addressable.rb
+++ app/models/concerns/addressable.rb
@@ -7,8 +7,18 @@ module Concerns
     end
 
     module ClassMethods
-      def address_categories(categories, default: 'default')
+      def address_categories(categories, default: nil)
+        # TODO move these to a Addressable::Config just like storable
+        # @address_categories = categories.map(&:to_sym)
+        # @default_address_category = default.present? ? default.to_sym : @address_categories.first
       end
+    end
+
+    def address(category = nil)
+      # category given?
+      # category allowed?
+      # find or initialize
+      # make sure to only find/initialize once
     end
   end
 end

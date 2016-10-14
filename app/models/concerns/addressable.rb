f325026388ce40f6da1897bbfe57c4375d58b3b4
Bauffman <davy@bauffman.be>
2016-10-14 16:03:07 +0200

    Finish addressable concern.

--- app/models/concerns/addressable.rb
+++ app/models/concerns/addressable.rb
@@ -7,18 +7,34 @@ module Concerns
     end
 
     module ClassMethods
-      def address_categories(categories, default: nil)
-        # TODO move these to a Addressable::Config just like storable
-        # @address_categories = categories.map(&:to_sym)
-        # @default_address_category = default.present? ? default.to_sym : @address_categories.first
+      def configure_adress(categories, default: nil)
+        address_config.update(categories, default: default)
+      end
+
+      def address_config
+        @address_config ||= Concerns::Addressable::Config.new
       end
     end
 
     def address(category = nil)
-      # category given?
-      # category allowed?
-      # find or initialize
-      # make sure to only find/initialize once
+      category = self.class.address_config.default unless category.present?
+
+      unless self.class.address_config.allowed?(category)
+        raise "You can't select the '#{category}' address because it's not an allowed category"
+      end
+
+      @address_collections = {} unless @address_collections
+
+      unless @address_collections[category.to_sym]
+
+        @address_collections[category.to_sym] = Address.find_or_initialize_by(
+          category: category,
+          addressable_type: self.class.name,
+          addressable_id: id
+        )
+      end
+
+      @address_collections[category.to_sym]
     end
   end
 end

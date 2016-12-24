a15c072f073a1a9ac3f65b0cb38a87cabd687cde
Bauffman <davy@bauffman.be>
2016-12-24 14:21:21 +0100

    Cleanup.

--- spec/routing/backend/general_spec.rb
+++ spec/routing/backend/general_spec.rb
@@ -2,9 +2,6 @@ require 'rails_helper'
 
 describe 'general routes' do
   it 'GET /backend' do
-    expect(get: backend_path).to route_to(
-                                   controller: 'backend/dashboard',
-                                   action: 'show'
-                                 )
+    expect(get: backend_path).to route_to('backend/dashboard#show')
   end
 end

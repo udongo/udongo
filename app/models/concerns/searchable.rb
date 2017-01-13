53f918ab88b2c79e618b374d8f8dacd91c847d30
davelens <github@davelens.be>
2017-01-13 17:28:57 +0100

    provide Concerns::Searchable#after_create functionality to create an index when a searchable relation is created

--- app/models/concerns/searchable.rb
+++ app/models/concerns/searchable.rb
@@ -4,6 +4,50 @@ module Concerns
 
     included do
       has_many :search_indices, as: :searchable, dependent: :destroy
+
+      # TODO: callbacks.
+      # Create indices for every indicated searchable field.
+      # Update them when their searchable values change.
+      # Make sure to take translations into account.
+      after_create do
+        self.class.searchable_fields_list.each do |key|
+          if translatable?
+            next unless self.class.translatable_fields_list.include?(key)
+
+            Udongo.config.i18n.app.locales.each do |locale|
+              value = translation(locale.to_sym).send(key)
+              next if value.blank?
+              search_indices.create!(locale: locale, key: key, value: value)
+            end
+          else
+            search_indices.create!(
+              locale: Udongo.config.i18n.app.default_locale,
+              key: key,
+              value: send(key)
+            )
+          end
+        end
+      end
+
+      # TODO:
+      after_save do
+      end
+    end
+
+    module ClassMethods
+      def searchable_field(key)
+        unless searchable_fields_list.include?(key.to_sym)
+          searchable_fields_list << key.to_sym
+        end
+      end
+
+      def searchable_fields(*args)
+        args.each { |key| searchable_field(key) }
+      end
+
+      def searchable_fields_list
+        @searchable_fields_list ||= []
+      end
     end
   end
 end
--- spec/support/concerns/searchable.rb
+++ spec/support/concerns/searchable.rb
@@ -3,6 +3,68 @@ require 'rails_helper'
 shared_examples_for :searchable do
   let(:klass) { described_class.to_s.underscore.to_sym }
 
+  describe 'after_create' do
+    before(:each) do
+      allow(described_class).to receive(:searchable_fields_list) { [:foo] }
+    end
+
+    context 'non-translatable model' do
+      before(:each) do
+        allow_any_instance_of(described_class).to receive(:translatable?) { false }
+        allow_any_instance_of(described_class).to receive(:foo) { 'bar' }
+      end
+
+      it 'default' do
+        instance = build(klass)
+        expect(instance.search_indices).to eq []
+      end
+
+      it 'creates a search index' do
+        instance = create(klass)
+        expect(instance.search_indices.find_by(locale: :nl, key: 'foo').value).to eq 'bar'
+      end
+    end
+
+    context 'translatable model' do
+      before(:each) do
+        described_class.translatable_fields :foo, :bar
+      end
+
+      let(:create_instance!) do
+        instance = build(klass)
+        Udongo.config.i18n.app.locales.each do |l|
+          t = instance.translation(l.to_sym)
+          t.foo = "baz #{l}"
+          t.bar = 'bak'
+        end
+        instance.save
+        instance
+      end
+
+      it 'creates search indices for every translation' do
+        instance = create_instance!
+        expect(instance.search_indices.find_by(locale: :nl, key: 'foo').value).to eq 'baz nl'
+        expect(instance.search_indices.find_by(locale: :fr, key: 'foo').value).to eq 'baz fr'
+        expect(instance.search_indices.find_by(locale: :en, key: 'foo').value).to eq 'baz en'
+      end
+
+      it 'does not copy translatable fields that are not in searchable fields' do
+        instance = create_instance!
+        expect(instance.search_indices.find_by(locale: :nl, key: 'bar')).to be nil
+      end
+    end
+  end
+
+  describe 'after_save' do
+    # TODO:
+  end
+
+  it '.respond_to?' do
+    expect(described_class).to respond_to(
+      :searchable_field, :searchable_fields, :searchable_fields_list
+    )
+  end
+
   it '#respond_to?' do
     expect(described_class.new).to respond_to(:search_indices)
   end

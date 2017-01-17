db2035f31b89bd303e69b80158e3de8527b531cf
davelens <github@davelens.be>
2017-01-17 15:43:53 +0100

    add indices to SearchIndex

--- spec/dummy/db/schema.rb
+++ spec/dummy/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170112151235) do
+ActiveRecord::Schema.define(version: 20170117143805) do
 
   create_table "addresses", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
     t.integer  "addressable_id"
@@ -258,6 +258,8 @@ ActiveRecord::Schema.define(version: 20170112151235) do
     t.text     "value",           limit: 65535
     t.datetime "created_at",                    null: false
     t.datetime "updated_at",                    null: false
+    t.index ["locale", "key"], name: "index_search_indices_on_locale_and_key", using: :btree
+    t.index ["searchable_type", "searchable_id"], name: "index_search_indices_on_searchable_type_and_searchable_id", using: :btree
   end
 
   create_table "settings", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|

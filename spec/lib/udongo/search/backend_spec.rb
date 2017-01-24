76ce59e3ed5d5d97d680e9706a42e983faf7ef3b
davelens <github@davelens.be>
2017-01-24 16:11:07 +0100

    refactor Udongo::Search::Backend spec

--- spec/lib/udongo/search/backend_spec.rb
+++ spec/lib/udongo/search/backend_spec.rb
@@ -4,18 +4,28 @@ describe Udongo::Search::Backend do
   let(:klass) { described_class.to_s.underscore.to_sym }
   let(:instance) { described_class.new('foo') }
 
+  before(:each) do
+    class Udongo::Search::ResultObjects::Foo < Udongo::Search::ResultObject
+    end
+
+    create(:search_module, name: 'Foo', weight: 1)
+  end
+
   describe '#find' do
-    # TODO:
+    it 'default' do
+      expect(instance.find).to eq []
+    end
+
+    it 'single result' do
+      # TODO:
+    end
+
+    it 'multiple results' do
+      # TODO:
+    end
   end
 
   describe '#result_object' do
-    before(:each) do
-      class Udongo::Search::ResultObjects::Foo < Udongo::Search::ResultObject
-      end
-
-      create(:search_module, name: 'Foo', weight: 1)
-    end
-
     let(:index) do
       create(:search_index, searchable_type: 'Foo', searchable_id: 5, value: 'foo')
     end

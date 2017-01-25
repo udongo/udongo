9a2441ffdce64cbed370ff31734e7db6b34befd1
davelens <github@davelens.be>
2017-01-25 15:10:02 +0100

    add todo clarifying the FlexibleContent problem in conjunction with Udongo::Search::Base

--- app/models/concerns/searchable.rb
+++ app/models/concerns/searchable.rb
@@ -5,6 +5,18 @@ module Concerns
     included do
       has_many :search_indices, as: :searchable, dependent: :destroy
 
+      # TODO: What on earth am I going to do with FlexibleContent?
+      #
+      # A possible solution could have been to make ContentText searchable,
+      # but because that model is not polymorphic, this is not possible.
+      #
+      # Another solution in the realms of the feasible:
+      # In theory, if I could save the ContentText#content value as an index
+      # linked to the parent Page object, there would be no problem.
+      # The Udongo::Search::Base#indices method already groups by searchable
+      # so in this case there wouldn't be duplicates, only additional matches
+      # for the searchable because it takes ContentText#content into account.
+
       # Creates and saves indices for every indicated searchable field.
       # Takes translations into account.
       after_save do

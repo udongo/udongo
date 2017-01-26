8dd7a9109d933e1d0c5328ced7d65feafc203467
davelens <github@davelens.be>
2017-01-26 17:03:31 +0100

    provide another clarifying comment regarding search result object extendability

--- lib/udongo/search/result_objects/page.rb
+++ lib/udongo/search/result_objects/page.rb
@@ -1,7 +1,10 @@
 module Udongo::Search::ResultObjects
   class Page < Udongo::Search::ResultObject
-    # This class could be used to perform further mutations on any of the data
-    # provided in Udongo::Search::ResultObject.
+    # This class should be used to perform further mutations on any of the data
+    # provided in Udongo::Search::ResultObject. A controller context class
+    # is accessible through #controller in order to have access to routes.
+    # The #namespace method is there to help you differentiate values between
+    # frontend, backend or other namespaces.
     #
     # Example: If an autocomplete requires data from an external source
     # or another, unrelated model to be rendered in the partial, one could

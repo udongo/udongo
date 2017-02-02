fdd4900fcaf641d624f16e56369b478d263f93c6
davelens <github@davelens.be>
2017-02-02 18:15:01 +0100

    move ContentText#parent to included ContentType and refactor its #column method to be more concise

--- app/models/content_text.rb
+++ app/models/content_text.rb
@@ -5,21 +5,11 @@ class ContentText < ApplicationRecord
   # in turn updates all search indices.
   after_save { parent.save! if linked_to_searchable_parent? }
 
-  # TODO: Move this to ContentType
-  def column
-    ContentColumn.find_by(content: self)
-  end
-
   def content_type
     :text
   end
 
   def linked_to_searchable_parent?
     column.present? && parent.present? && parent.searchable?
-  end
-
-  # TODO: Move this to ContentType
-  def parent
-    column.row.rowable
   end
 end

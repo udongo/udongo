5f817ff0fc6b26e823e100f046f8a2194c6af7e0
Bauffman <davy@bauffman.be>
2017-02-06 11:28:16 +0100

    Fix some factories.

--- spec/factories/search_indices.rb
+++ spec/factories/search_indices.rb
@@ -2,6 +2,5 @@ FactoryGirl.define do
   factory :search_index do
     locale 'nl'
     name 'foo'
-    value 'bar'
   end
 end
--- spec/factories/search_modules.rb
+++ spec/factories/search_modules.rb
@@ -1,7 +1,5 @@
 FactoryGirl.define do
   factory :search_module do
     name 'Page'
-    searchable true
-    weight 1
   end
 end

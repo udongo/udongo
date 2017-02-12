111d7126d5d8bc0bbaf4a585b3641c1a04cd0674
Bauffman <davy@bauffman.be>
2017-02-12 15:56:38 +0100

    Fix alignment.

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,7 +4,7 @@ class User < ActiveRecord::Base
 
   validates :first_name, :last_name, presence: true
   validates :email,
-    presence: true,
-    uniqueness: { case_sensitive: false },
-    format: { with: /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\Z/i }
+            presence: true,
+            uniqueness: { case_sensitive: false },
+            format: { with: /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\Z/i }
 end

eb5280102cb2b1df516c8b892579931f91500cff
Bauffman <davy@bauffman.be>
2017-02-12 15:58:04 +0100

    Get rid of the user decorator.

--- app/decorators/user_decorator.rb
+++
@@ -1,9 +0,0 @@
-class UserDecorator < ApplicationDecorator
-  delegate_all
-
-  def abbreviation
-    base = display_name.present? ? display_name : full_name
-    return email if base.blank?
-    base.split(' ').map(&:first).join('')
-  end
-end

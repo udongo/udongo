30c86e687c773eb666c5012888732f3f3079175f
Bauffman <davy@bauffman.be>
2017-02-12 15:57:00 +0100

    Simplify checking a user for a valid email address.

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,6 +5,6 @@ class User < ActiveRecord::Base
   validates :first_name, :last_name, presence: true
   validates :email,
             presence: true,
-            uniqueness: { case_sensitive: false },
-            format: { with: /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\Z/i }
+            email: true,
+            uniqueness: { case_sensitive: false }
 end

928d4a033c58736bd81fa07ae84ecb18f7ea2646
Bauffman <davy@bauffman.be>
2017-02-15 14:48:15 +0100

    Simplify the page translation form (with seo).

--- app/forms/backend/page_translation_form.rb
+++ app/forms/backend/page_translation_form.rb
@@ -1,68 +1,10 @@
-class Backend::PageTranslationForm < Udongo::Form
-  attr_reader :page, :translation, :seo
-
+class Backend::PageTranslationForm < Backend::TranslationWithSeoForm
   attribute :title, String
   attribute :subtitle, String
 
-  attribute :seo_title, String
-  attribute :seo_keywords, String
-  attribute :seo_description, String
-  attribute :seo_custom, String
-  attribute :seo_slug, String
-
-  validates :title, :seo_slug, presence: true
-
-  delegate :id, to: :page
-
-  def initialize(page, translation, seo)
-    @page = page
-    @translation = translation
-    @seo = seo
-
-    self.title = @translation.title
-    self.subtitle = @translation.subtitle
-
-    %w(title keywords description custom slug).each do |f|
-      self.send("seo_#{f}=", @seo.send(f))
-    end
-  end
+  validates :title, presence: true
 
   def self.model_name
     Page.model_name
-  end
-
-  def persisted?
-    true
-  end
-
-  def save(params)
-    self.title = params[:title]
-    self.subtitle = params[:subtitle]
-
-    %w(title keywords description custom slug).each do |f|
-      self.send("seo_#{f}=", params["seo_#{f}".to_sym])
-    end
-
-    if valid?
-      save_object
-      true
-    else
-      false
-    end
-  end
-
-  private
-
-  def save_object
-    @translation.title = title
-    @translation.subtitle = subtitle
-
-    %w(title keywords description custom slug).each do |f|
-      @seo.send("#{f}=", send("seo_#{f}"))
-    end
-
-    # Saves the page, translation and SEO info all at once.
-    @seo.save!
-    @page.save!
   end
 end

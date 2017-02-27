e408e6da2965f2a66edd98f14f017cbfa4a1577f
Bauffman <davy@bauffman.be>
2017-02-27 23:49:26 +0100

    Add some very basic automatic resizing.

--- app/models/asset.rb
+++ app/models/asset.rb
@@ -11,8 +11,25 @@ class Asset < ApplicationRecord
     content_type.include?('image')
   end
 
+  def image_url(width = nil, height = nil, action: :resize_to_limit, options: {})
+    return unless image?
+
+    Udongo::Assets::Resize.new(self).url(
+      width,
+      height,
+      action: action,
+      options: options
+    )
+  end
+
+  # TODO remove?
   def extension
     read_attribute(:filename).split('.').last
+  end
+
+  # TODO keep?
+  def name_with_extension
+    read_attribute(:filename)
   end
 
   private

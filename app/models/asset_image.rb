e704e460bd9f06da5118582774ad1c4b7615c86b
Bauffman <davy@bauffman.be>
2017-02-28 13:29:29 +0100

    Add 2 asset resize actions.

--- app/models/asset_image.rb
+++ app/models/asset_image.rb
@@ -25,23 +25,16 @@ class AssetImage
     str << "-q#{options[:quality]}" if options[:quality]
     # TODO add gravity if present
     str << "-#{width}x#{height}"
-    str << "-#{@asset.read_attribute(:filename)}"
+    str << "-#{@asset.actual_filename}"
     str
   end
 
   def url(width = nil, height = nil, options = {})
     options[:action] = :resize_to_limit unless options.key?(:action)
 
-    md5 = Digest::MD5.hexdigest(@asset.id.to_s)
-    main_dir = md5[0,2]
-    second_dir = md5[2,2]
-
-    # if width/height are nil, you need to fetch the original file!
-
     if width.nil? && height.nil?
-      return "/uploads/assets/#{main_dir}/#{second_dir}/#{@asset.read_attribute(:filename)}"
+      return "/uploads/assets/#{main_dir}/#{second_dir}/#{@asset.actual_filename}"
     end
-
 
     name = filename(width, height, options)
 
@@ -52,7 +45,7 @@ class AssetImage
         when :resize_to_limit
           resize_to_limit(width, height, options)
         else
-          raise 'foo'
+          resize_to_fit(width, height, options)
       end
     end
 
@@ -64,12 +57,13 @@ class AssetImage
     "#{Rails.root}/public#{url(width, height, options)}"
   end
 
+  # Resize the image to fit within the specified dimensions while retaining
+  # the original aspect ratio. Will only resize the image if it is larger than the
+  # specified dimensions. The resulting image may be shorter or narrower than specified
+  # in the smaller dimension but will not be larger than the specified values.
+  #
   def resize_to_limit(width, height, options = {})
     options[:action] = :resize_to_limit
-
-    md5 = Digest::MD5.hexdigest(@asset.id.to_s)
-    main_dir = md5[0,2]
-    second_dir = md5[2,2]
 
     name = filename(width, height, options)
 
@@ -82,22 +76,50 @@ class AssetImage
     img.write("#{Rails.root}/public/uploads/assets/_cache/#{main_dir}/#{second_dir}/#{name}")
   end
 
-  def resize_to_fit(width, height)
-    # [width]x[height]
+  # Resize the image to fit within the specified dimensions while retaining
+  # the original aspect ratio. The image may be shorter or narrower than
+  # specified in the smaller dimension but will not be larger than the specified values.
+  #
+  def resize_to_fit(width, height, options = {})
+    options[:action] = :resize_to_fit
 
-    # resize picture
-    #
+    name = filename(width, height, options)
+
+    img = MiniMagick::Image.open(@asset.filename.path)
+    img.combine_options do |c|
+      c.quality(options[:quality]) if options[:quality]
+      c.resize "#{width}x#{height}"
+    end
+
+    img.write("#{Rails.root}/public/uploads/assets/_cache/#{main_dir}/#{second_dir}/#{name}")
   end
 
+  # Resize the image to fit within the specified dimensions while retaining
+  # the aspect ratio of the original image. If necessary, crop the image in the
+  # larger dimension.
+  #
   def resize_to_fill(width, height, gravity = 'Center')
     # [width]
     # OR
     # x[height]
   end
 
+  # Resize the image to fit within the specified dimensions while retaining
+  # the original aspect ratio. If necessary, will pad the remaining area
+  # with the given color, which defaults to transparent (for gif and png,
+  # white for jpeg).
+  #
   def resize_and_pad(width, height, gravity = 'Center', background = :transparant)
     # [width]x[height]
     # :transparant background for png
     # :white background for jpg
+  end
+
+  def main_dir
+    @main_dir ||= Digest::MD5.hexdigest(@asset.id.to_s)[0,2]
+  end
+
+  def second_dir
+    @second_dir ||= Digest::MD5.hexdigest(@asset.id.to_s)[2,2]
   end
 end

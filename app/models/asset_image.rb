69a06f6a9610e2b12d2885e1d1e6da8f9d79923a
Bauffman <davy@bauffman.be>
2017-02-28 14:47:04 +0100

    Use the right background var.

--- app/models/asset_image.rb
+++ app/models/asset_image.rb
@@ -155,7 +155,7 @@ class AssetImage
     img.combine_options do |cmd|
       cmd.thumbnail "#{width}x#{height}>"
 
-      if options[:background].to_sym == :transparent
+      if background.to_sym == :transparent
         cmd.background 'rgba(255, 255, 255, 0.0)'
       else
         cmd.background background

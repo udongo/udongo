a9365f78a795cdafefc198b66ff9080077fd32bf
davelens <github@davelens.be>
2017-03-03 13:56:00 +0100

    refactor Udongo::CarrierwaveVersioningOnDemand

--- lib/udongo/assets/carrierwave_versioning_on_demand.rb
+++ lib/udongo/assets/carrierwave_versioning_on_demand.rb
@@ -6,35 +6,39 @@
 # url/path params.
 module Udongo::CarrierwaveVersioningOnDemand
   def path(options = {})
-    options.reverse_merge!(action: :resize_to_limit, width: nil, height: nil)
-    width, height = [options[:width], options[:height]]
-    return super() if width.nil? && height.nil?
+    options.reverse_merge!(default_options)
+    return super() if options[:width].nil? && options[:height].nil?
     url(options) # Trigger resize
-    version_base(:path, width, height)
+    version_base(:path, options)
   end
 
   def url(options = {})
-    options.reverse_merge!(action: :resize_to_limit, width: nil, height: nil)
-    width, height = [options[:width], options[:height]]
-    return super(options[:version]) if width.nil? && height.nil?
-    path_to_version = version_base(path, width, height)
-
-    unless File.exists?(path_to_version)
-      case options[:action].to_sym
-      when :resize_to_limit then resize_to_limit(width, height) { |img| img.write(path_to_version);img }
-      when :resize_to_fit then resize_to_fit(width, height) { |img| img.write(path_to_version);img }
-      when :resize_to_fill then resize_to_fill(width, height) { |img| img.write(path_to_version);img }
-      when :resize_and_pad then resize_and_pad(width, height) { |img| img.write(path_to_version);img }
-      else
-        raise "No such resize action '#{options[:action].to_s}'. Available are: resize_to_limit, resize_to_fit, resize_to_fill and resize_and_pad."
-      end
-    end
-
-    version_base(:url, width, height)
+    options.reverse_merge!(default_options)
+    return super(options[:version]) if options[:width].nil? && options[:height].nil?
+    trigger_resize(version_base(path, options), options)
+    version_base(:url, options)
   end
 
-  def version_base(type = :url, width, height)
-    filename = "#{width}_#{height}_#{file.filename}"
+  private
+
+  def default_options
+    { action: :resize_to_limit, width: nil, height: nil }
+  end
+
+  def trigger_resize(path_to_version, options = {})
+    unless File.exists?(path_to_version)
+      # The block allows us to override the location of the written file.
+      self.send(options[:action], options[:width], options[:height]) do |img|
+        img.write(path_to_version)
+        img
+      end
+    end
+  rescue
+    raise "No such resize action '#{options[:action].to_s}'. Available are: resize_to_limit, resize_to_fit, resize_to_fill and resize_and_pad."
+  end
+
+  def version_base(type = :url, options = {})
+    filename = "#{options[:width]}_#{options[:height]}_#{file.filename}"
     return File.join('/', store_dir, filename) if type.to_sym == :url
     File.join(Rails.root, 'public', store_dir, filename) # path requested
   end

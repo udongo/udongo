a4afd90d8a657f7c9998fe82f7ad3ea77e695edd
Bauffman <davy@bauffman.be>
2017-03-05 12:57:54 +0100

    Move resize to fill to a separate class.

--- app/models/asset_image.rb
+++ app/models/asset_image.rb
@@ -98,34 +98,11 @@ class AssetImage
   # NorthWest, North, NorthEast, West, Center, East, SouthWest, South, SouthEast
   #
   def resize_to_fill(width, height, options = {})
-    gravity = options.key?(:gravity) ? options[:gravity] : 'Center'
-
-    img = MiniMagick::Image.open(@asset.filename.path)
-    cols, rows = img[:dimensions]
-
-    img.combine_options do |cmd|
-      if width != cols || height != rows
-        scale_x = width/cols.to_f
-        scale_y = height/rows.to_f
-
-        if scale_x >= scale_y
-          cols = (scale_x * (cols + 0.5)).round
-          rows = (scale_x * (rows + 0.5)).round
-          cmd.resize "#{cols}"
-        else
-          cols = (scale_y * (cols + 0.5)).round
-          rows = (scale_y * (rows + 0.5)).round
-          cmd.resize "x#{rows}"
-        end
-      end
-
-      cmd.quality options[:quality] if options.key?(:quality)
-      cmd.gravity gravity
-      cmd.background 'rgba(255,255,255,0.0)'
-      cmd.extent "#{width}x#{height}" if cols != width || rows != height
-    end
-
-    img.write(actual_path(filename(width, height, options)))
+    Udongo::ImageManipulation::ResizeToFill.new(
+      @asset.filename.path, width, height, options
+    ).resize(
+      actual_path(filename(width, height, options))
+    )
   end
 
   # Resize the image to fit within the specified dimensions while retaining

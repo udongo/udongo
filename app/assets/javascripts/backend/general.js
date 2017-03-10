815dee03464a9a0fc0033897dd2e0877538be098
davelens <github@davelens.be>
2017-03-10 14:57:30 +0100

    mark form inputs and dropdowns as dirty whenever they get a change event triggered

--- app/assets/javascripts/backend/general.js
+++ app/assets/javascripts/backend/general.js
@@ -1,6 +1,11 @@
 var general = general || {
   init: function() {
     $('form:not(.no-focus) input:visible:not(.no-focus):first').focus();
+    $('input, select').change(this.input_change_listener);
+  },
+
+  input_change_listener: function(e) {
+    $(this).data('dirty', true);
   }
 };
 

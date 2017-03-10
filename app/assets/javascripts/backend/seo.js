8b67b0bea23d744c223b463588a22314e275b3e9
davelens <github@davelens.be>
2017-03-10 14:33:42 +0100

    adds an parameterized autofill for the slug field when typing a designated field (defaults to title)

---
+++ app/assets/javascripts/backend/seo.js
@@ -0,0 +1,31 @@
+var seo = {
+  init: function() {
+    this.sluggable_input().on('keyup', this.keyup_listener);
+  },
+
+  data: function(key) {
+    return $('[data-seo]').data(key);
+  },
+
+  keyup_listener: function(e) {
+    if(seo.slug_input().attr('disabled')) return;
+
+    $.ajax({
+      url: seo.data('path'),
+      data: { value: $(this).val() },
+      type: 'GET'
+    }).done(function(data){
+      seo.slug_input().val(data.result);
+    });
+  },
+
+  slug_input: function() {
+    return $('input[id*="seo_slug"]');
+  },
+
+  sluggable_input: function() {
+    return $($('[data-seo]').data('sluggable-input'));
+  }
+};
+
+$(function(){ seo.init(); });

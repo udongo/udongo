fd173a8ad3ae19eeb9279ec254505c32ddc310e4
davelens <github@davelens.be>
2017-03-13 11:19:16 +0100

    add overridable message parameter to Backend::FormHelper.trigger_dirty_inputs_warning

--- app/helpers/backend/form_helper.rb
+++ app/helpers/backend/form_helper.rb
@@ -1,5 +1,5 @@
 module Backend::FormHelper
-  def trigger_dirty_inputs_warning
-    content_tag :span, t('b.msg.dirty_inputs_warning'), class: 'hidden-xs-up', data: { dirty: false }
+  def trigger_dirty_inputs_warning(message: I18n.t('b.msg.dirty_inputs_warning'))
+    content_tag :span, message, class: 'hidden-xs-up', data: { dirty: false }
   end
 end
--- spec/helpers/backend/form_helper_spec.rb
+++ spec/helpers/backend/form_helper_spec.rb
@@ -1,9 +1,18 @@
 require 'rails_helper'
 
 describe Backend::FormHelper do
-  it '.trigger_dirty_inputs_warning' do
-    I18n.locale = :nl
-    expect(trigger_dirty_inputs_warning).to eq "<span class=\"hidden-xs-up\" data-dirty=\"false\">Je gaat de pagina verlaten maar je aanpassingen zijn nog niet bewaard.\nWil je nog steeds de pagina verlaten?</span>"
+  describe '.trigger_dirty_inputs_warning' do
+    before(:each) do
+      I18n.locale = :nl
+    end
+
+    it 'default' do
+      expect(trigger_dirty_inputs_warning).to eq "<span class=\"hidden-xs-up\" data-dirty=\"false\">Je gaat de pagina verlaten maar je aanpassingen zijn nog niet bewaard.\nWil je nog steeds de pagina verlaten?</span>"
+    end
+
+    it 'with custom message' do
+      expect(trigger_dirty_inputs_warning(message: 'foo')).to eq "<span class=\"hidden-xs-up\" data-dirty=\"false\">foo</span>"
+    end
   end
 
   it '.responds_to?' do

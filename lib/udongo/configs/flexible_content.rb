d29b1d788e2a292b937eb63d32d19cf4d14300bf
Bauffman <davy@bauffman.be>
2017-04-13 20:18:57 +0200

    Add some todos.

--- lib/udongo/configs/flexible_content.rb
+++ lib/udongo/configs/flexible_content.rb
@@ -4,10 +4,14 @@ module Udongo
       include Virtus.model
 
       BREAKPOINTS = %w(xs sm md lg xl)
+      DEFAULT_BREAKPOINT = 'md'
 
       attribute :types, Array, default: %w(text picture image)
+
+      # TODO remove this
       attribute :allowed_breakpoints, Array, default: BREAKPOINTS
 
+      # TODO remove this
       def allowed_breakpoint?(value)
         allowed_breakpoints.include?(value.to_s)
       end

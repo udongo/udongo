b48e31277059ef7136e37ca4731ba833ea0fd379
Bauffman <davy@bauffman.be>
2017-05-04 11:28:42 +0200

    Make the link helper a lot more flexible.

--- app/helpers/link_helper.rb
+++ app/helpers/link_helper.rb
@@ -1,30 +1,47 @@
 module LinkHelper
-  def link_to_show(object)
-    str = Udongo::ObjectPath.find(object)
+  def link_to_show(value)
+    if value.is_a?(String)
+      url = value
+    else
+      str = Udongo::ObjectPath.find(value)
+      url = send(str, *Udongo::ObjectPath.remove_symbols(value))
+    end
+
+    # str = Udongo::ObjectPath.find(object)
 
     link_to(
       icon(:search),
-      send(str, *Udongo::ObjectPath.remove_symbols(object)),
+      url,
       title: t('b.view')
     )
   end
 
-  def link_to_edit(object)
-    str = "edit_#{Udongo::ObjectPath.find(object)}"
+  def link_to_edit(value)
+    if value.is_a?(String)
+      url = value
+    else
+      str = "edit_#{Udongo::ObjectPath.find(value)}"
+      url = send(str, *Udongo::ObjectPath.remove_symbols(value))
+    end
 
     link_to(
       icon(:pencil_square_o),
-      send(str, *Udongo::ObjectPath.remove_symbols(object)),
+      url,
       title: t('b.edit')
     )
   end
 
-  def link_to_delete(object)
-    str = Udongo::ObjectPath.find(object)
+  def link_to_delete(value)
+    if value.is_a?(String)
+      url = value
+    else
+      str = Udongo::ObjectPath.find(value)
+      url = send(str, *Udongo::ObjectPath.remove_symbols(value))
+    end
 
     link_to(
       icon(:trash),
-      send(str, *Udongo::ObjectPath.remove_symbols(object)),
+      url,
       method: :delete,
       data: { confirm: t('b.msg.confirm') },
       title: t('b.delete')
--- spec/helpers/link_helper_spec.rb
+++ spec/helpers/link_helper_spec.rb
@@ -8,18 +8,43 @@ describe LinkHelper do
     @admin = create(:admin, id: 37)
   end
 
-  it '#link_to_show' do
-    expected = '<a title="Bekijk" href="/backend/admins/37"><i class="fa fa-search"></i></a>'
-    expect(link_to_show([:backend, @admin])).to eq expected
+  describe '#link_to_show' do
+    it 'object param' do
+      expected = '<a title="Bekijk" href="/backend/admins/37"><i class="fa fa-search"></i></a>'
+      expect(link_to_show([:backend, @admin])).to eq expected
+    end
+
+    it 'string param' do
+      expected = '<a title="Bekijk" href="/backend/admins/37"><i class="fa fa-search"></i></a>'
+      expect(link_to_show(backend_admin_path(@admin))).to eq expected
+    end
   end
 
-  it '#link_to_edit' do
-    expected = '<a title="Bewerk" href="/backend/admins/37/edit"><i class="fa fa-pencil-square-o"></i></a>'
-    expect(link_to_edit([:backend, @admin])).to eq expected
+  describe '#link_to_edit' do
+    it 'object param' do
+      expected = '<a title="Bewerk" href="/backend/admins/37/edit"><i class="fa fa-pencil-square-o"></i></a>'
+      expect(link_to_edit([:backend, @admin])).to eq expected
+    end
+
+    it 'string param' do
+      expected = '<a title="Bewerk" href="/backend/admins/37/edit"><i class="fa fa-pencil-square-o"></i></a>'
+      expect(link_to_edit(edit_backend_admin_path(@admin))).to eq expected
+    end
   end
 
-  it '#link_to_delete' do
-    expected = '<a data-confirm="Ben je zeker?" title="Verwijder" rel="nofollow" data-method="delete" href="/backend/admins/37"><i class="fa fa-trash"></i></a>'
-    expect(link_to_delete([:backend, @admin])).to eq expected
+  describe '#link_to_delete' do
+    it 'object param' do
+      expected = '<a data-confirm="Ben je zeker?" title="Verwijder" rel="nofollow" data-method="delete" href="/backend/admins/37"><i class="fa fa-trash"></i></a>'
+      expect(link_to_delete([:backend, @admin])).to eq expected
+    end
+
+    it 'string param' do
+      expected = '<a data-confirm="Ben je zeker?" title="Verwijder" rel="nofollow" data-method="delete" href="/backend/admins/37"><i class="fa fa-trash"></i></a>'
+      expect(link_to_delete(backend_admin_path(@admin))).to eq expected
+    end
+  end
+
+  it '#respond_to?' do
+    expect(self).to respond_to(:link_to_show, :link_to_edit, :link_to_delete)
   end
 end

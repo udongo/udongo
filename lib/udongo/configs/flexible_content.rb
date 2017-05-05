acfab8cfae1e12266d9b41c3628dad24b115364e
Bauffman <davy@bauffman.be>
2017-05-05 15:48:12 +0200

    Remove the allowed_breakpoints list.

--- lib/udongo/configs/flexible_content.rb
+++ lib/udongo/configs/flexible_content.rb
@@ -7,14 +7,6 @@ module Udongo
       DEFAULT_BREAKPOINT = 'md'
 
       attribute :types, Array, default: %w(text picture image)
-
-      # TODO remove this
-      attribute :allowed_breakpoints, Array, default: BREAKPOINTS
-
-      # TODO remove this
-      def allowed_breakpoint?(value)
-        allowed_breakpoints.include?(value.to_s)
-      end
     end
   end
 end

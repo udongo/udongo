875bf6b00a7d97661cc3628e01abbf62191611b1
Bauffman <davy@bauffman.be>
2017-05-05 16:18:50 +0200

    Tweak the js a bit.

--- app/assets/javascripts/backend/flexible_content_breakpoints.js
+++ app/assets/javascripts/backend/flexible_content_breakpoints.js
@@ -1,9 +1,23 @@
 var flexible_content_breakpoints = flexible_content_breakpoints || {
     init: function() {
       $('a[data-show-all-breakpoints=true]').click(function(){
-        $(this).hide();
-        $('[data-advanced-breakpoint=true]').show();
+        flexible_content_breakpoints.show_all_breakpoints();
       });
+
+      $('[data-default-breakpoint] select').change(function(){
+        flexible_content_breakpoints.adjust_widths();
+      });
+    },
+
+    show_all_breakpoints: function() {
+      var label = $('[data-default-breakpoint=true] label');
+      label.text(label.text() + ' (medium)');
+
+      $('a[data-show-all-breakpoints=true]').hide();
+      $('[data-advanced-breakpoint=true]').show();
+    },
+
+    adjust_widths: function() {
     }
   };
 

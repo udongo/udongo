76f940d3969c05890e6873239c1404e5af7a44a3
Bauffman <davy@bauffman.be>
2017-05-06 15:08:55 +0200

    Make the breakpoints change based on the medium value.

--- app/assets/javascripts/backend/flexible_content_breakpoints.js
+++ app/assets/javascripts/backend/flexible_content_breakpoints.js
@@ -1,24 +1,32 @@
 var flexible_content_breakpoints = flexible_content_breakpoints || {
-    init: function() {
-      $('a[data-show-all-breakpoints=true]').click(function(){
-        flexible_content_breakpoints.show_all_breakpoints();
-      });
+  init: function() {
+    $('a[data-show-all-breakpoints=true]').click(function(){
+      flexible_content_breakpoints.show_all_breakpoints();
+    });
 
-      $('[data-default-breakpoint] select').change(function(){
-        flexible_content_breakpoints.adjust_widths();
-      });
-    },
+    $('[data-default-breakpoint] select').change(function(){
+      flexible_content_breakpoints.adjust_widths();
+    });
+  },
 
-    show_all_breakpoints: function() {
-      var label = $('[data-default-breakpoint=true] label');
-      label.text(label.text() + ' (medium)');
+  show_all_breakpoints: function() {
+    var label = $('[data-default-breakpoint=true] label');
+    label.text(label.text() + ' (medium)');
 
-      $('a[data-show-all-breakpoints=true]').hide();
-      $('[data-advanced-breakpoint=true]').show();
-    },
+    $('a[data-show-all-breakpoints=true]').hide();
+    $('[data-advanced-breakpoint=true]').show();
+  },
 
-    adjust_widths: function() {
-    }
-  };
+  adjust_widths: function() {
+    // TODO make sure this method is not executed once the advanced button is clicked.
+
+    var value = parseInt($('[data-default-breakpoint=true] select').val());
+
+    $('[data-breakpoint-xs=true] select').val(12);
+    $('[data-breakpoint-sm=true] select').val(12);
+    $('[data-breakpoint-lg=true] select').val(value);
+    $('[data-breakpoint-xl=true] select').val(value);
+  }
+};
 
 $(function(){ flexible_content_breakpoints.init(); });

291f375442077d25f48cc9ebf8c9113abcb0e2cf
Bauffman <davy@bauffman.be>
2017-05-16 12:08:12 +0200

    Add nasty short_name method to Person.

--- app/models/concerns/person.rb
+++ app/models/concerns/person.rb
@@ -3,5 +3,18 @@ module Concerns
     def full_name
       "#{first_name} #{last_name}"
     end
+
+    def short_name
+      s = ''
+      s << "#{first_name}" if first_name.present?
+      s << ' ' if first_name.present? && last_name.present?
+
+      last_name.to_s.split(' ').each do |chunk|
+        s << chunk[0,1]
+        s << '.'
+      end
+
+      s
+    end
   end
 end
--- spec/support/concerns/person.rb
+++ spec/support/concerns/person.rb
@@ -9,7 +9,34 @@ shared_examples_for :person do
     expect(person.full_name).to eq 'Foo Bar'
   end
 
+  describe '#short_name' do
+    it 'no first name' do
+      person = build(klass, first_name: nil, last_name: 'Bar')
+      expect(person.short_name).to eq 'B.'
+    end
+
+    it 'no last name' do
+      person = build(klass, first_name: 'Foo', last_name: nil)
+      expect(person.short_name).to eq 'Foo'
+    end
+
+    it 'last name with 1 word' do
+      person = build(klass, first_name: 'Foo', last_name: 'Bar')
+      expect(person.short_name).to eq 'Foo B.'
+    end
+
+    it 'last name with 2 words' do
+      person = build(klass, first_name: 'Foo', last_name: 'Bar Baz')
+      expect(person.short_name).to eq 'Foo B.B.'
+    end
+
+    it 'last name with 3 words' do
+      person = build(klass, first_name: 'Foo', last_name: 'Bar Baz Boz')
+      expect(person.short_name).to eq 'Foo B.B.B.'
+    end
+  end
+
   it '#respond_to?' do
-    expect(model.new).to respond_to(:full_name)
+    expect(model.new).to respond_to(:full_name, :short_name)
   end
 end

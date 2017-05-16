be21571534c798ce08e41157df1b2149e4b311f6
Bauffman <davy@bauffman.be>
2017-05-16 13:48:17 +0200

    Add setting to easily see if the picture caption editor is allowed.

--- spec/lib/udongo/configs/flexible_content_spec.rb
+++ spec/lib/udongo/configs/flexible_content_spec.rb
@@ -12,11 +12,28 @@ describe Udongo::Configs::FlexibleContent do
     it :types do
       expect(instance.types).to eq %w(text picture video image)
     end
+
+    it :picture_caption_editor do
+      expect(instance.picture_caption_editor).to eq false
+    end
+  end
+
+  describe '#picture_caption_editor?' do
+    it :true do
+      instance.picture_caption_editor = true
+      expect(instance.picture_caption_editor?).to eq true
+    end
+
+    it :false do
+      instance.picture_caption_editor = false
+      expect(instance.picture_caption_editor?).to eq false
+    end
   end
 
   it '#respond_to?' do
     expect(instance).to respond_to(
-      :types, :types=
+      :types, :types=, :picture_caption_editor, :picture_caption_editor=,
+      :picture_caption_editor?
     )
   end
 end

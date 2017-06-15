195e83e01bdde8f87cc9b7d57ba7a57f7cbd8fd3
Bauffman <davy@bauffman.be>
2017-06-15 15:28:04 +0200

    Add the content slideshow decorator.

---
+++ app/decorators/content_slideshow_decorator.rb
@@ -0,0 +1,3 @@
+class ContentSlideshowDecorator < ApplicationDecorator
+  delegate_all
+end
---
+++ spec/decorators/content_slideshow_decorator_spec.rb
@@ -0,0 +1,7 @@
+require 'rails_helper'
+
+describe ContentSlideshowDecorator do
+  it '#respond_to?' do
+    # expect(build(:content_form).decorate).to respond_to()
+  end
+end

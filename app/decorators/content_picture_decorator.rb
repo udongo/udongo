e90754a9c8dc97d7a4c1e8619108abbbe6542903
Bauffman <davy@bauffman.be>
2017-06-25 16:40:52 +0200

    Refactor the frontend flexible content picture to optimize the image sizes.

--- app/decorators/content_picture_decorator.rb
+++ app/decorators/content_picture_decorator.rb
@@ -1,3 +1,7 @@
 class ContentPictureDecorator < ApplicationDecorator
   delegate_all
+
+  def max_image_size
+    (1600.0 / 12.0 * column.width_xl.to_f).to_i
+  end
 end
--- spec/decorators/content_picture_decorator_spec.rb
+++ spec/decorators/content_picture_decorator_spec.rb
@@ -1,7 +1,71 @@
 require 'rails_helper'
 
 describe ContentPictureDecorator do
+  describe '#max_image_size' do
+    let(:picture) { create(:content_picture).decorate }
+
+    it 'content width xl: 1' do
+      create(:content_column, content: picture, width_xl: 1)
+      expect(picture.max_image_size).to eq 133
+    end
+
+    it 'content width xl: 2' do
+      create(:content_column, content: picture, width_xl: 2)
+      expect(picture.max_image_size).to eq 266
+    end
+
+    it 'content width xl: 3' do
+      create(:content_column, content: picture, width_xl: 3)
+      expect(picture.max_image_size).to eq 400
+    end
+
+    it 'content width xl: 4' do
+      create(:content_column, content: picture, width_xl: 4)
+      expect(picture.max_image_size).to eq 533
+    end
+
+    it 'content width xl: 5' do
+      create(:content_column, content: picture, width_xl: 5)
+      expect(picture.max_image_size).to eq 666
+    end
+
+    it 'content width xl: 6' do
+      create(:content_column, content: picture, width_xl: 6)
+      expect(picture.max_image_size).to eq 800
+    end
+
+    it 'content width xl: 7' do
+      create(:content_column, content: picture, width_xl: 7)
+      expect(picture.max_image_size).to eq 933
+    end
+
+    it 'content width xl: 8' do
+      create(:content_column, content: picture, width_xl: 8)
+      expect(picture.max_image_size).to eq 1066
+    end
+
+    it 'content width xl: 9' do
+      create(:content_column, content: picture, width_xl: 9)
+      expect(picture.max_image_size).to eq 1200
+    end
+
+    it 'content width xl: 10' do
+      create(:content_column, content: picture, width_xl: 10)
+      expect(picture.max_image_size).to eq 1333
+    end
+
+    it 'content width xl: 11' do
+      create(:content_column, content: picture, width_xl: 11)
+      expect(picture.max_image_size).to eq 1466
+    end
+
+    it 'content width xl: 12' do
+      create(:content_column, content: picture, width_xl: 12)
+      expect(picture.max_image_size).to eq 1600
+    end
+  end
+
   it '#respond_to?' do
-    # expect(build(:content_image).decorate).to respond_to()
+    expect(build(:content_picture).decorate).to respond_to(:max_image_size)
   end
 end

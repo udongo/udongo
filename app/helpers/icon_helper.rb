137e1278d87493b874ec35acfce8a8a8604e2882
davelens <github@davelens.be>
2017-11-30 10:50:38 +0100

    clarify why a named label parameter is not a good idea for now

--- app/helpers/icon_helper.rb
+++ app/helpers/icon_helper.rb
@@ -1,6 +1,16 @@
 module IconHelper
-  def icon(name, label = nil)
+  # I wanted to make a named param out of label, though this would imply a
+  # backwards incompatible change that would require a non-trivial amount of
+  # editing.
+  #
+  # If you want to take this along with the next planned major release, you can
+  # use the following CMD in bash (assuming you have silver searcher installed)
+  # to get a list of all icon calls that pass anything more than just a name.
+  #
+  #   ag 'icon\((:|")(\w+), '
+  #
+  def icon(name, lbl = nil)
     markup = content_tag :i, nil, class: "fa fa-#{name.to_s.tr('_', '-')}"
-    label ? "#{markup}&nbsp;#{label}".html_safe : markup
+    lbl ? "#{markup}&nbsp;#{lbl}".html_safe : markup
   end
 end

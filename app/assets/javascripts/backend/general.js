523e61abf39cce4c9c61cb3b02f00b5befdeafe0
davelens <github@davelens.be>
2017-11-30 17:49:53 +0100

    add preliminary code to trigger a tab-like experience for the button_radios input

--- app/assets/javascripts/backend/general.js
+++ app/assets/javascripts/backend/general.js
@@ -3,6 +3,15 @@ var general = general || {
     $('form:not(.no-focus) input:visible:not(.no-focus):first').focus();
     $('[data-toggle="tooltip"]').tooltip();
     $('[href="#lity-close"]').on('click', this.lity_close_click_listener);
+    $('.form_toggle label').on('click', this.form_toggle_click_listener);
+  },
+
+  // TODO: See if this can be made generic
+  form_toggle_click_listener: function(e) {
+    var form = $(this).parents('form');
+    var input = $(this).find('input');
+    $('[data-button-radio-toggle]').addClass('d-none');
+    $('.form_'+ input.val()).removeClass('d-none');
   },
 
   lity_close_click_listener: function(e) {

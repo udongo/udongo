a3046a0acf9999e3fc0a0c6444b6125e2fd58964
davelens <github@davelens.be>
2017-11-30 11:22:32 +0100

    shorten syntax in IconHelper tests

--- spec/helpers/icon_helper_spec.rb
+++ spec/helpers/icon_helper_spec.rb
@@ -3,36 +3,30 @@ require 'rails_helper'
 describe IconHelper do
   describe '#icon' do
     it 'name as symbol' do
-      output = icon(:foo)
-      expect(output).to eq '<i class="fa fa-foo"></i>'
+      expect(icon(:foo)).to eq '<i class="fa fa-foo"></i>'
     end
 
     it 'name as string' do
-      output = icon('foo')
-      expect(output).to eq '<i class="fa fa-foo"></i>'
+      expect(icon('foo')).to eq '<i class="fa fa-foo"></i>'
     end
 
     it 'name with underscores' do
-      output = icon(:foo_bar_baz)
-      expect(output).to eq '<i class="fa fa-foo-bar-baz"></i>'
+      expect(icon(:foo_bar_baz)).to eq '<i class="fa fa-foo-bar-baz"></i>'
     end
 
     context 'when passing a label' do
       it 'it returns the icon HTML with a label' do
-        output = icon(:foo, 'Bar')
-        expect(output).to eq '<i class="fa fa-foo"></i>&nbsp;Bar'
+        expect(icon(:foo, 'Bar')).to eq '<i class="fa fa-foo"></i>&nbsp;Bar'
       end
 
       it 'returns an instance of ActiveSupport::SafeBuffer' do
-        output = icon(:foo, 'Bar')
-        expect(output).to be_a(ActiveSupport::SafeBuffer)
+        expect(icon(:foo, 'Bar')).to be_a(ActiveSupport::SafeBuffer)
       end
     end
 
     context 'when not passing a label' do
       it 'returns an instance of ActiveSupport::SafeBuffer' do
-        output = icon(:foo, 'Bar')
-        expect(output).to be_a(ActiveSupport::SafeBuffer)
+        expect(icon(:foo, 'Bar')).to be_a(ActiveSupport::SafeBuffer)
       end
     end
 

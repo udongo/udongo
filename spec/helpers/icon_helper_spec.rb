21e5f3b7b95dcf4cd534f1100795f82512b5f128
davelens <github@davelens.be>
2017-11-30 10:46:32 +0100

    remove checking for a resulting ActiveSupport::SafeBuffer instance from every test but the ones that matter for IconHelper

--- spec/helpers/icon_helper_spec.rb
+++ spec/helpers/icon_helper_spec.rb
@@ -5,25 +5,35 @@ describe IconHelper do
     it 'name as symbol' do
       output = icon(:foo)
       expect(output).to eq '<i class="fa fa-foo"></i>'
-      expect(output).to be_a(ActiveSupport::SafeBuffer)
     end
 
     it 'name as string' do
       output = icon('foo')
       expect(output).to eq '<i class="fa fa-foo"></i>'
-      expect(output).to be_a(ActiveSupport::SafeBuffer)
     end
 
     it 'name with underscores' do
       output = icon(:foo_bar_baz)
       expect(output).to eq '<i class="fa fa-foo-bar-baz"></i>'
-      expect(output).to be_a(ActiveSupport::SafeBuffer)
     end
 
-    it 'with a label' do
-      output = icon(:foo, 'Bar')
-      expect(output).to eq '<i class="fa fa-foo"></i>&nbsp;Bar'
-      expect(output).to be_a(ActiveSupport::SafeBuffer)
+    context 'when passing a label' do
+      it 'it returns the icon HTML with a label' do
+        output = icon(:foo, 'Bar')
+        expect(output).to eq '<i class="fa fa-foo"></i>&nbsp;Bar'
+      end
+
+      it 'returns an instance of ActiveSupport::SafeBuffer' do
+        output = icon(:foo, 'Bar')
+        expect(output).to be_a(ActiveSupport::SafeBuffer)
+      end
+    end
+
+    context 'when not passing a label' do
+      it 'returns an instance of ActiveSupport::SafeBuffer' do
+        output = icon(:foo, 'Bar')
+        expect(output).to be_a(ActiveSupport::SafeBuffer)
+      end
     end
   end
 end

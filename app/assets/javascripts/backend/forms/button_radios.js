fed6e7d94dc3ac1cce690c85b3633ff1ca48ba4b
davelens <github@davelens.be>
2017-12-01 17:58:07 +0100

    alphabetize

--- app/assets/javascripts/backend/forms/button_radios.js
+++ app/assets/javascripts/backend/forms/button_radios.js
@@ -10,16 +10,6 @@ forms.button_radios = forms.button_radios || {
     return $('[data-br="'+ label.data('target') +'"]');
   },
 
-  label_click_listener: function(e) {
-    var target = forms.button_radios.data_target_for_label($(this));
-
-    forms.button_radios.hide_content();
-    target.show();
-    setTimeout(function(){
-      target.find('input').focus();
-    }, 50);
-  },
-
   hide_content: function() {
     $('.button_radios label').each(function(){
       var label = $(this);
@@ -29,6 +19,16 @@ forms.button_radios = forms.button_radios || {
         if(!label.hasClass('active')) target.hide();
       }, 50);
     });
+  },
+
+  label_click_listener: function(e) {
+    var target = forms.button_radios.data_target_for_label($(this));
+
+    forms.button_radios.hide_content();
+    target.show();
+    setTimeout(function(){
+      target.find('input').focus();
+    }, 50);
   },
 
   label_iterator: function() {

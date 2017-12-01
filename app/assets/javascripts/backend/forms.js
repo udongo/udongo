ff9fecae581d66b5cbb430642c9da7918cf0e3a8
davelens <github@davelens.be>
2017-12-01 15:53:05 +0100

    add focus to first input when clicking a button radio label

--- app/assets/javascripts/backend/forms.js
+++ app/assets/javascripts/backend/forms.js
@@ -1,15 +1,19 @@
 var forms = forms || {
   bind_events: function() {
-    this.hide_button_radio_targets();
     this.give_focus_to_first_visible_input();
+    this.hide_button_radio_targets();
     $('.button_radios label').on('click', this.button_radio_label_click_listener)
   },
 
   button_radio_label_click_listener: function(e) {
     var label = $(this);
     var target = $(label.data('target'));
+
     forms.hide_button_radio_targets();
     target.show();
+    setTimeout(function(){
+      target.find('input').focus();
+    }, 50);
   },
 
   hide_button_radio_targets: function() {

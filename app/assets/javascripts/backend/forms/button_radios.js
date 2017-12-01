d77f46df4575c71d0c69628fc690fbe1df4ae8cc
davelens <github@davelens.be>
2017-12-01 18:21:48 +0100

    make the button radios JS a bit simpler still

--- app/assets/javascripts/backend/forms/button_radios.js
+++ app/assets/javascripts/backend/forms/button_radios.js
@@ -12,30 +12,29 @@ forms.button_radios = forms.button_radios || {
 
   hide_content: function() {
     $('.button_radios label').each(function(){
-      var label = $(this);
-      var target = forms.button_radios.data_target_for_label(label);
-
-      setTimeout(function(){
-        if(!label.hasClass('active')) target.hide();
-      }, 50);
+      forms.button_radios
+        .data_target_for_label($(this))
+        .hide();
     });
   },
 
   label_click_listener: function(e) {
     var target = forms.button_radios.data_target_for_label($(this));
+    forms.button_radios.hide_content(); // Hide all content first...
+    forms.button_radios.show_content(target); // ... before showing the active one.
+  },
 
-    forms.button_radios.hide_content();
+  // This determines if content for a given radio button may be visible.
+  label_iterator: function() {
+    var label = $(this);
+    var target = forms.button_radios.data_target_for_label(label);
+    if(label.hasClass('active')) forms.button_radios.show_content(target);
+  },
+
+  show_content: function(target) {
     target.show();
     setTimeout(function(){
       target.find('input').focus();
     }, 50);
-  },
-
-  label_iterator: function() {
-    var label = $(this);
-    var target = forms.button_radios.data_target_for_label(label);
-    if(label.hasClass('active')) target.show();
-    label.on('click', forms.button_radios.label_click_listener);
   }
-
 };

9f2f6ac528998b72ddc7e5f9195123aa37dae407
davelens <github@davelens.be>
2017-12-01 16:19:02 +0100

    clean up ButtonRadiosInput whilst allowing for wrapper/input html options

--- app/inputs/button_radios_input.rb
+++ app/inputs/button_radios_input.rb
@@ -1,13 +1,13 @@
 class ButtonRadiosInput < SimpleForm::Inputs::Base
   def input(wrapper_options = nil)
-    template.content_tag :div, class: 'btn-group', data: { toggle: 'buttons' } do
+    template.content_tag :div, wrapper_options do
       string = ''
 
       input_options[:collection].each do |value|
         active = object.send(attribute_name) == value.to_s
 
         string << template.content_tag(:label, class: "btn btn-primary #{'active' if active}", data: { target: "[data-#{object_name}-#{value}]".gsub('_', '-') }) do
-          content = template.radio_button_tag("#{object_name}[#{attribute_name}]", value, active)
+          content = @builder.radio_button(attribute_name, value, input_html_options)
           content << template.t("simple_form.labels.#{object_name}.#{value}")
           content
         end

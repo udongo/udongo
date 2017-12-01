9847861df87812903c991ba78d02d46d8f75b46b
davelens <github@davelens.be>
2017-12-01 16:07:04 +0100

    make DatePickerInput spec fast and refactor some syntax

--- spec/inputs/date_picker_input_spec.rb
+++ spec/inputs/date_picker_input_spec.rb
@@ -1,25 +1,24 @@
-require 'rails_helper'
-
 describe DatePickerInput do
-  let(:instance) { described_class.new(OpenStruct.new, 'foo', 'foo', 'text') }
+  subject { described_class.new(OpenStruct.new, 'foo', 'foo', 'text') }
+
   before(:each) do
-    allow(instance).to receive(:template) { ActionView::Base.new }
+    allow(subject).to receive(:template) { ActionView::Base.new }
   end
 
   it '#data_attributes' do
     allow(I18n).to receive(:locale) { :nl }
-    expect(instance.data_attributes).to eq({
+    expect(subject.data_attributes).to eq({
       date_language: :nl,
       date_format: 'dd/mm/yyyy'
     })
   end
 
   it '#icon_table' do
-    expect(instance.icon_table).to eq '<span class="fa fa-th"></span>'
+    expect(subject.icon_table).to eq '<span class="fa fa-th"></span>'
   end
 
   it '#responds_to?' do
-    expect(instance).to respond_to(
+    expect(subject).to respond_to(
       :input, :data_attributes, :icon_table, :input_html_options, :span_table
     )
   end

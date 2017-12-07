37c0b6c7246a03c406c38ef59e1bb7336deb7b7c
davelens <github@davelens.be>
2017-12-07 16:38:22 +0100

    add Form#code_snippet to b/forms#edit_translation

--- app/forms/backend/form_translation_form.rb
+++ app/forms/backend/form_translation_form.rb
@@ -1,9 +1,9 @@
 class Backend::FormTranslationForm < Backend::TranslationForm
   attribute :summary, String
-
   attribute :toggle, String
   attribute :redirect_url, String
   attribute :success_message, String
+  attribute :code_snippet, String
 
   def self.model_name
     Form.model_name

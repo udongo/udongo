f207330c4937b9b89810d19483463b86fbf2c585
davelens <github@davelens.be>
2018-03-30 10:39:35 +0200

    remove presence validation from Backend::TagTranslationForm#summary

--- app/forms/backend/tag_translation_form.rb
+++ app/forms/backend/tag_translation_form.rb
@@ -1,8 +1,6 @@
 class Backend::TagTranslationForm < Backend::TranslationWithSeoForm
   attribute :summary, String
 
-  validates :summary, presence: true
-
   def self.model_name
     Tag.model_name
   end

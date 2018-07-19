7d4a094f83c1cd2ab3c8879bc7e0eb23af8e660a
davelens <github@davelens.be>
2018-07-19 19:57:08 +0200

    update the actor for the notice indicating when all search terms were destroyed

--- app/controllers/backend/search_terms_controller.rb
+++ app/controllers/backend/search_terms_controller.rb
@@ -9,6 +9,6 @@ class Backend::SearchTermsController < Backend::BaseController
 
   def destroy_all
     ::SearchTerm.destroy_all
-    redirect_to backend_search_terms_path, notice: translate_notice(:deleted, :search)
+    redirect_to backend_search_terms_path, notice: translate_notice(:deleted, :search_terms)
   end
 end

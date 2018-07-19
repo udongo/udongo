5fc459c1428e3deb834e3f998a795e16b1d0f336
davelens <github@davelens.be>
2018-07-19 19:03:21 +0200

    order used search terms by count

--- app/controllers/backend/search_terms_controller.rb
+++ app/controllers/backend/search_terms_controller.rb
@@ -4,6 +4,7 @@ class Backend::SearchTermsController < Backend::BaseController
   def index
     @search_terms = ::SearchTerm.select('*, COUNT(1) as total')
                                 .group('locale, term')
+                                .order('COUNT(1) DESC')
   end
 
   def destroy_all

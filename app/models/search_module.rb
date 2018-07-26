b1147509b01225ad7d5aa9593d332de454b94e16
davelens <github@davelens.be>
2018-07-26 13:00:42 +0200

    housekeeping

--- app/models/search_module.rb
+++ app/models/search_module.rb
@@ -5,6 +5,6 @@ class SearchModule < ApplicationRecord
 
   def indices
     SearchIndex.joins('INNER JOIN search_modules ON search_indices.searchable_type = search_modules.name')
-      .where('search_modules.name = ?', name)
+               .where('search_modules.name = ?', name)
   end
 end

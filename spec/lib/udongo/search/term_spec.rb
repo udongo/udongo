443be1df9780a425a67234976629e0504d1170f9
davelens <github@davelens.be>
2018-10-04 10:22:40 +0200

    use the newer subject{} syntax instead of let(:instance), part 2

--- spec/lib/udongo/search/term_spec.rb
+++ spec/lib/udongo/search/term_spec.rb
@@ -9,65 +9,65 @@ describe Udongo::Search::Term do
   end
 
   describe '#locale' do
-    let(:instance) { described_class.new('foobar') }
+    subject { described_class.new('foobar') }
 
     it 'defaults to Udongo.config.i18n.app.default_locale without controller input' do
       allow(Udongo.config.i18n.app).to receive(:default_locale) { 'jp' }
-      expect(instance.locale).to eq :jp
+      expect(subject.locale).to eq :jp
     end
 
     it 'value passed from controller' do
       controller = OpenStruct.new(locale: :en)
-      instance = described_class.new('foobar', controller: controller)
-      expect(instance.locale).to eq :en
+      subject = described_class.new('foobar', controller: controller)
+      expect(subject.locale).to eq :en
     end
   end
 
   describe '#synonym' do
     it 'nil' do
-      instance = described_class.new('Unknown term')
-      expect(instance.synonym).to be nil
+      subject = described_class.new('Unknown term')
+      expect(subject.synonym).to be nil
     end
 
     it 'matches single synonym' do
-      instance = described_class.new('developer')
-      expect(instance.synonym).to eq @synonym_a
+      subject = described_class.new('developer')
+      expect(subject.synonym).to eq @synonym_a
     end
 
     describe 'finds a match in a comma-separated list of 3 synonyms' do
       it 'matches on first' do
-        instance = described_class.new('blub')
-        expect(instance.synonym).to eq @synonym_b
+        subject = described_class.new('blub')
+        expect(subject.synonym).to eq @synonym_b
       end
 
       it 'matches on second' do
-        instance = described_class.new('blubber')
-        expect(instance.synonym).to eq @synonym_b
+        subject = described_class.new('blubber')
+        expect(subject.synonym).to eq @synonym_b
       end
 
       it 'matches on third' do
-        instance = described_class.new('blubst')
-        expect(instance.synonym).to eq @synonym_b
+        subject = described_class.new('blubst')
+        expect(subject.synonym).to eq @synonym_b
       end
     end
 
     it 'matches locale' do
       controller = OpenStruct.new(locale: 'fr')
-      instance = described_class.new('le search', controller: controller)
+      subject = described_class.new('le search', controller: controller)
 
-      expect(instance.synonym).to eq @synonym_d
+      expect(subject.synonym).to eq @synonym_d
     end
   end
 
   describe '#value' do
     it 'repeats incoming term without synonym match' do
-      instance = described_class.new('Unknown term')
-      expect(instance.value).to eq 'Unknown term'
+      subject = described_class.new('Unknown term')
+      expect(subject.value).to eq 'Unknown term'
     end
 
     it 'returns synonym term after synonym match' do
-      instance = described_class.new('blubber')
-      expect(instance.value).to eq 'foobar'
+      subject = described_class.new('blubber')
+      expect(subject.value).to eq 'foobar'
     end
   end
 

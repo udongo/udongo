7bb158e00e1c91268fd4a9547179b8c443b14dd7
davelens <github@davelens.be>
2018-10-19 17:40:15 +0200

    add leading slash functionality to Redirect#destination_uri as well

--- app/models/redirect.rb
+++ app/models/redirect.rb
@@ -8,7 +8,8 @@ class Redirect < ApplicationRecord
   before_validation do
     # Adding a leading slash at this point makes sure the uniqueness validation
     # keeps working.
-    self.source_uri = source_uri.gsub(/^(?!\/)/, '/') if source_uri.present?
+    self.source_uri = add_leading_slash(source_uri)
+    self.destination_uri = add_leading_slash(destination_uri)
   end
 
   def enabled?
@@ -28,5 +29,11 @@ class Redirect < ApplicationRecord
     response = Udongo::Redirects::Test.new(self).perform!(base_url: base_url)
     return next_in_chain.works?(base_url: base_url) if next_in_chain.present?
     response.redirect_works?(base_url + destination_uri)
+  end
+
+  private
+
+  def add_leading_slash(value)
+    value.to_s.gsub(/^(?!\/)/, '/') if value.present?
   end
 end
--- spec/decorators/redirect_decorator_spec.rb
+++ spec/decorators/redirect_decorator_spec.rb
@@ -1,10 +1,10 @@
 require 'rails_helper'
 
 describe RedirectDecorator do
-  let(:instance) { build(:redirect).decorate }
+  subject { build(:redirect).decorate }
 
   it '#status_code_collection' do
-    expect(instance.status_code_collection).to eq [
+    expect(subject.status_code_collection).to eq [
                                                     ['301 (Moved Permanently)', '301'],
                                                     ['303 (See Other)', '303'],
                                                     ['307 (Temporary Redirect)', '307']
@@ -15,11 +15,11 @@ describe RedirectDecorator do
     redirect = create(:redirect, source_uri: 'foo', destination_uri: 'bar').decorate
 
     I18n.with_locale :nl do
-      expect(redirect.summary).to eq 'Van foo naar bar'
+      expect(redirect.summary).to eq 'Van /foo naar /bar'
     end
   end
 
   it '#respond_to?' do
-    expect(instance).to respond_to(:status_code_collection, :summary)
+    expect(subject).to respond_to(:status_code_collection, :summary)
   end
 end

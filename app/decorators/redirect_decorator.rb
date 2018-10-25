510216b8dbfc99b747f9854910403d29d47aa798
davelens <github@davelens.be>
2018-10-25 11:37:26 +0200

    rework the redirects index to be more readable whilst incorporating the redirect working status

--- app/decorators/redirect_decorator.rb
+++ app/decorators/redirect_decorator.rb
@@ -1,6 +1,10 @@
 class RedirectDecorator < ApplicationDecorator
   delegate_all
 
+  def status_badge
+    Udongo::Redirects::StatusBadge.new(h, object).html
+  end
+
   def status_code_collection
     %w(301 303 307).map do |code|
       [I18n.t("b.msg.status_codes.#{code}"), code]

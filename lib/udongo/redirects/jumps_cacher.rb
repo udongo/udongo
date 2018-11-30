5b87e32e3865d9e874e1d70677b474638e7f9b59
davelens <github@davelens.be>
2018-11-30 11:23:02 +0100

    make sure the module Udongo::Redirects gets loaded correctly in production environments

--- lib/udongo/redirects/jumps_cacher.rb
+++ lib/udongo/redirects/jumps_cacher.rb
@@ -1,17 +1,19 @@
-class Udongo::Redirects::JumpsCacher
-  def initialize(redirect)
-    @redirect = redirect
-  end
+module Udongo::Redirects
+  class JumpsCacher
+    def initialize(redirect)
+      @redirect = redirect
+    end
 
-  def cache!
-    top_most_redirect.trace_down.each(&:cache_jumps!)
-  end
+    def cache!
+      top_most_redirect.trace_down.each(&:cache_jumps!)
+    end
 
-  def top_most_redirect
-    @redirect.trace_up.first
-  end
+    def top_most_redirect
+      @redirect.trace_up.first
+    end
 
-  def already_on_top?
-    @redirect == top_most_redirect
+    def already_on_top?
+      @redirect == top_most_redirect
+    end
   end
 end
